eee2e10cbaf2111b4842f00fec2d9b638aa7ca4b
Magnus Norddahl <dpjudas@github.com>
2012-10-27 21:31:12 +0000

    - Added some tag names and ids

--- Tests/GUI/CSSDocument2/program.cpp
+++ Tests/GUI/CSSDocument2/program.cpp
@@ -22,12 +22,23 @@ int Program::main(const std::vector<std::string> &args)
 	window_desc.set_allow_resize(true);
 
 	GUIComponent *root = new GUIComponent(&gui, window_desc);
+	root->set_tag_name("root");
 	root->func_close().set(&Program::on_close, root);
 	GUIComponent *child1 = new GUIComponent(root);
+	child1->set_tag_name("child");
+	child1->set_id("child1");
 	GUIComponent *child11 = new GUIComponent(child1);
+	child11->set_tag_name("child");
+	child11->set_id("child11");
 	GUIComponent *child2 = new GUIComponent(root);
+	child2->set_tag_name("child");
+	child2->set_id("child2");
 	GUIComponent *child22 = new GUIComponent(child2);
+	child22->set_tag_name("child");
+	child22->set_id("child22");
 	GUIComponent *child23 = new GUIComponent(child2);
+	child23->set_tag_name("child");
+	child23->set_id("child23");
 
 	gui.exec();
 
