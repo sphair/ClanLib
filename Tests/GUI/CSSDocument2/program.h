96d86b0d729d369d77d507f26b74356d364dde8a
Magnus Norddahl <dpjudas@github.com>
2012-10-31 22:46:38 +0000

    - Hooked up resize

--- Tests/GUI/CSSDocument2/program.h
+++ Tests/GUI/CSSDocument2/program.h
@@ -6,4 +6,5 @@ class Program
 public:
 	static int main(const std::vector<std::string> &args);
 	static bool on_close(clan::GUIComponent *component);
+	static void on_resized(clan::GUIComponent *component);
 };
