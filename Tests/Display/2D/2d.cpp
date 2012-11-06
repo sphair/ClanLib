71cd172cfe1492f76fdc53e16dba29ba87eadfe1
Griswoldz <Grizmu@gmail.com>
2012-11-06 11:31:19 +0000

    Updated Display/2D test for the ClanLib 3.0

--- Tests/Display/2D/2d.cpp
+++ Tests/Display/2D/2d.cpp
@@ -87,13 +87,18 @@ int App::start(const std::vector<std::string> &args)
 		// Connect a keyboard handler to on_key_up()
 		Slot slot_input_up = (window.get_ic().get_keyboard()).sig_key_up().connect(this, &App::on_input_up);
 
-		// Get the graphic context
-		GraphicContext gc = window.get_gc();
+		// Create the Canvas
+		Canvas canvas(window);
+
+		//Set the reference to GraphicContext
+		GraphicContext &gc = canvas.get_gc();
 
 		Texture2D texture_image(gc, "tux.png");
-		texture_image.set_wrap_mode(cl_wrap_repeat, cl_wrap_repeat);
-		texture_image.set_min_filter(cl_filter_linear);
-		texture_image.set_mag_filter(cl_filter_linear);
+		texture_image.set_wrap_mode(wrap_repeat, wrap_repeat);
+		texture_image.set_min_filter(filter_linear);
+		texture_image.set_mag_filter(filter_linear);
+
+		Image image(gc,texture_image,texture_image.get_size());
 
 		ResourceManager resources("resources.xml");
 		Sprite sprite(gc, "test", &resources);
@@ -111,21 +116,21 @@ int App::start(const std::vector<std::string> &args)
 		// Run until someone presses escape
 		while (!quit)
 		{
-			gc.clear(Colorf(0.0f,0.0f,0.2f));
+			canvas.clear(Colorf(0.0f,0.0f,0.2f));
 
-			gc.set_map_mode(MapMode(cl_map_2d_upper_left));
+			canvas.set_map_mode(MapMode(map_2d_upper_left));
 
-	// Draw::point()
+	// Canvas::point()
 			for (int xcnt=0; xcnt<8; xcnt++)
 			{
 				for (int ycnt=0; ycnt<6; ycnt++)
 				{
-					Draw::point(gc, xcnt*2, ycnt*2, Colorf(1.0f, 1.0f, 1.0f, 1.0f));
+					canvas.point(xcnt*2, ycnt*2, Colorf(1.0f, 1.0f, 1.0f, 1.0f));
 				}
 			}
-			small_font.draw_text(gc, 32, 10, "8*6 Points (0 + 2x), (0 + 2y)");
+			small_font.draw_text(canvas, 32, 10, "8*6 Points (0 + 2x), (0 + 2y)");
 
-	// Draw::line()
+	// Canvas::line()
 			for (int xcnt=0; xcnt<4; xcnt++)
 			{
 				for (int ycnt=0; ycnt<3; ycnt++)
@@ -133,12 +138,12 @@ int App::start(const std::vector<std::string> &args)
 					const int offset_y = 16;
 					const int line_length = 6;
 					const int spacing = 8;
-					Draw::line(gc, xcnt*spacing, (ycnt*spacing) + offset_y, line_length + (xcnt*spacing), (line_length + (ycnt*spacing)) + offset_y, Colorf(1.0f, 1.0f, 1.0f, 1.0f));
+					canvas.line(xcnt*spacing, (ycnt*spacing) + offset_y, line_length + (xcnt*spacing), (line_length + (ycnt*spacing)) + offset_y, Colorf(1.0f, 1.0f, 1.0f, 1.0f));
 				}
 			}
-			small_font.draw_text(gc, 48, 30, "4*3 Lines (0 + 8x), (32 + 8y), (6 + 8x), (38 + 8y)");
+			small_font.draw_text(canvas, 48, 30, "4*3 Lines (0 + 8x), (32 + 8y), (6 + 8x), (38 + 8y)");
 
-	// Draw::box()
+	// Canvas::box()
 			for (int xcnt=0; xcnt<4; xcnt++)
 			{
 				for (int ycnt=0; ycnt<3; ycnt++)
@@ -146,12 +151,12 @@ int App::start(const std::vector<std::string> &args)
 					const int offset_y = 48;
 					const int line_length = 6;
 					const int spacing = 8;
-					Draw::box(gc, xcnt*spacing, (ycnt*spacing) + offset_y, line_length + (xcnt*spacing), (line_length + (ycnt*spacing)) + offset_y, Colorf(1.0f, 1.0f, 1.0f, 1.0f));
+					canvas.box(xcnt*spacing, (ycnt*spacing) + offset_y, line_length + (xcnt*spacing), (line_length + (ycnt*spacing)) + offset_y, Colorf(1.0f, 1.0f, 1.0f, 1.0f));
 				}
 			}
-			small_font.draw_text(gc, 48, 66, "4*3 Box (0 + 8x), (32 + 8y), (6 + 8x), (38 + 8y)");
+			small_font.draw_text(canvas, 48, 66, "4*3 Box (0 + 8x), (32 + 8y), (6 + 8x), (38 + 8y)");
 
-	// Draw::fill()
+	// Canvas::fill()
 			for (int xcnt=0; xcnt<4; xcnt++)
 			{
 				for (int ycnt=0; ycnt<3; ycnt++)
@@ -159,12 +164,12 @@ int App::start(const std::vector<std::string> &args)
 					const int offset_y = 80;
 					const int line_length = 6;
 					const int spacing = 8;
-					Draw::fill(gc, xcnt*spacing, (ycnt*spacing) + offset_y, line_length + (xcnt*spacing), (line_length + (ycnt*spacing)) + offset_y, Colorf(1.0f, 1.0f, 1.0f, 1.0f));
+					canvas.fill(xcnt*spacing, (ycnt*spacing) + offset_y, line_length + (xcnt*spacing), (line_length + (ycnt*spacing)) + offset_y, Colorf(1.0f, 1.0f, 1.0f, 1.0f));
 				}
 			}
-			small_font.draw_text(gc, 48, 90, "4*3 Fill (0 + 8x), (32 + 8y), (6 + 8x), (38 + 8y)");
-
-	// Draw::gradient_fill()
+			small_font.draw_text(canvas, 48, 90, "4*3 Fill (0 + 8x), (32 + 8y), (6 + 8x), (38 + 8y)");
+		
+	// Canvas::gradient_fill()
 			Gradient gradient;
 			gradient.top_left = Colorf(1.0f, 1.0f, 1.0f, 1.0f);
 			gradient.top_right = Colorf(1.0f, 0.0f, 0.0f, 1.0f);
@@ -177,27 +182,27 @@ int App::start(const std::vector<std::string> &args)
 					const int offset_y = 110;
 					const int line_length = 6;
 					const int spacing = 8;
-					Draw::gradient_fill(gc, xcnt*spacing, (ycnt*spacing) + offset_y, line_length + (xcnt*spacing), (line_length + (ycnt*spacing)) + offset_y, gradient);
+					canvas.gradient_fill(xcnt*spacing, (ycnt*spacing) + offset_y, line_length + (xcnt*spacing), (line_length + (ycnt*spacing)) + offset_y, gradient);
 				}
 			}
-			small_font.draw_text(gc, 48, 115, "4*3 GradientFill (0 + 8x), (32 + 8y), (6 + 8x), (38 + 8y)");
-			small_font.draw_text(gc, 48, 125, "top left = white. top right = red");
-			small_font.draw_text(gc, 48, 135, "bottom left = blue. bottom right = green");
+			small_font.draw_text(canvas, 48, 115, "4*3 GradientFill (0 + 8x), (32 + 8y), (6 + 8x), (38 + 8y)");
+			small_font.draw_text(canvas, 48, 125, "top left = white. top right = red");
+			small_font.draw_text(canvas, 48, 135, "bottom left = blue. bottom right = green");
 
-	// Draw::circle()
+	// Canvas::circle()
 			{
 				const int offset_y = 140;
 				int radius = 5;
-				Draw::circle(gc, radius, offset_y + radius, radius, Colorf(1.0f, 1.0f, 1.0f, 1.0f));
+				canvas.circle(radius, offset_y + radius, radius, Colorf(1.0f, 1.0f, 1.0f, 1.0f));
 
 				const int offset_x = 16;
 				radius = 16;
-				Draw::circle(gc, offset_x + radius, offset_y + radius, radius, Colorf(1.0f, 1.0f, 1.0f, 1.0f));
+				canvas.circle(offset_x + radius, offset_y + radius, radius, Colorf(1.0f, 1.0f, 1.0f, 1.0f));
 			}
-			small_font.draw_text(gc, 54, 149, "Circle (5, 145) Radius = 5");
-			small_font.draw_text(gc, 54, 159, "Circle (32, 156) Radius = 16");
+			small_font.draw_text(canvas, 54, 149, "Circle (5, 145) Radius = 5");
+			small_font.draw_text(canvas, 54, 159, "Circle (32, 156) Radius = 16");
 
-	// Draw::gradient_circle()
+	// Canvas::gradient_circle()
 			{
 				Gradient gradient;
 				gradient.top_left = Colorf(1.0f, 1.0f, 1.0f, 1.0f);
@@ -209,53 +214,63 @@ int App::start(const std::vector<std::string> &args)
 				float radius = 17.0;
 				float xpos = radius;
 				float ypos = offset_y + radius;
-				Draw::gradient_circle(gc, Pointf( xpos, ypos ), Pointf(radius/2.0, 0.0f), radius, gradient);
+				canvas.gradient_circle(Pointf( xpos, ypos ), Pointf(radius/2.0, 0.0f), radius, gradient);
 
 				const int offset_x = 40;
 				radius = 17.0;
 				xpos = offset_x + radius;
 				ypos = offset_y + radius;
-				Draw::gradient_circle(gc, Pointf( xpos, ypos), Pointf(0.0f, radius/2.0), radius, gradient);
+				canvas.gradient_circle(Pointf( xpos, ypos), Pointf(0.0f, radius/2.0), radius, gradient);
 			}
 
-			small_font.draw_text(gc, 80, 189, "Gradient Circle (16, 196) Radius = 17. Gradient right");
-			small_font.draw_text(gc, 80, 199, "Gradient Circle (56, 196) Radius = 17. Gradient up");
-			small_font.draw_text(gc, 80, 209, "centre = white, outer = red");
+			small_font.draw_text(canvas, 80, 189, "Gradient Circle (16, 196) Radius = 17. Gradient right");
+			small_font.draw_text(canvas, 80, 199, "Gradient Circle (56, 196) Radius = 17. Gradient up");
+			small_font.draw_text(canvas, 80, 209, "centre = white, outer = red");
 	
-	// Draw::triangle()
+	// Canvas::triangle()
 			{
 				const float offset_y = 220.0f;
 				const float size = 12.0f;
-				Draw::triangle(gc, Pointf(0.0f, offset_y), Pointf(0.0f, offset_y + size), Pointf(size, offset_y + size), Colorf(1.0f, 1.0f, 1.0f, 1.0f));
+				canvas.triangle(Pointf(0.0f, offset_y), Pointf(0.0f, offset_y + size), Pointf(size, offset_y + size), Colorf(1.0f, 1.0f, 1.0f, 1.0f));
 
 				float offset_x = 16.0f;
-				Draw::triangle(gc, Pointf(offset_x + 0.0f, offset_y + size), Pointf(offset_x + size, offset_y + size), Pointf(offset_x + 0.0f, offset_y), Colorf(1.0f, 1.0f, 1.0f, 1.0f));
+				canvas.triangle(Pointf(offset_x + 0.0f, offset_y + size), Pointf(offset_x + size, offset_y + size), Pointf(offset_x + 0.0f, offset_y), Colorf(1.0f, 1.0f, 1.0f, 1.0f));
 
 				offset_x = 32.0f;
-				Draw::triangle(gc, Pointf(offset_x + size, offset_y + size), Pointf(offset_x + 0.0f, offset_y), Pointf(offset_x + 0.0f, offset_y + size), Colorf(1.0f, 1.0f, 1.0f, 1.0f));
+				canvas.triangle(Pointf(offset_x + size, offset_y + size), Pointf(offset_x + 0.0f, offset_y), Pointf(offset_x + 0.0f, offset_y + size), Colorf(1.0f, 1.0f, 1.0f, 1.0f));
 			}
-			small_font.draw_text(gc, 48, 229, "3 Triangles (12 pixel size) (Left vertical edge).");
-			small_font.draw_text(gc, 48, 239, "Top Left: (0,220)  (16,220)  (32,220)");
+			small_font.draw_text(canvas, 48, 229, "3 Triangles (12 pixel size) (Left vertical edge).");
+			small_font.draw_text(canvas, 48, 239, "Top Left: (0,220)  (16,220)  (32,220)");
 
-	// Draw::texture()
-			gc.set_texture(0, texture_image);
-			{
-				float offset_x = 0.0f;
-				float offset_y = 250.0f;
-				Rectf src_rect(offset_x, offset_y, Sizef(31, 47));
-				Rectf texture_coords(0.0, 0.0, 1.0f, 1.0f);
-				Colorf color(1.0f, 1.0f, 1.0f, 1.0f);
-				Draw::texture(gc, src_rect, color, texture_coords);
+	// Canvas::image()
 
-				offset_x = 33.0f;
-				src_rect = Rectf(offset_x, offset_y, Sizef(31, 47));
-				texture_coords = Rectf(0.25f, 0.25f, 0.75f, 0.75f);
-				color = Colorf(1.0f, 0.0f, 0.0f, 1.0f);
-				Draw::texture(gc, src_rect, color, texture_coords);
-			}
-			gc.reset_texture(0);
-			small_font.draw_text(gc, 76, 260, "Texture (0, 250) size=(31,47)");
-			small_font.draw_text(gc, 76, 275, "Texture (33, 250) size=(31,47) (red, magnify*2)");
+			Size image_size = image.get_size();
+			float offset_x = 0.0f;
+			float offset_y = 250.0f;
+			Rectf dest_rect= Rectf(offset_x, offset_y, Sizef(31, 47));
+			Rectf src_rect= Rectf(0.0*image_size.width, 
+								  0.0*image_size.width,
+								  1.0f*image_size.height,
+								  1.0f*image_size.height);
+			Colorf color(1.0f, 1.0f, 1.0f, 1.0f);
+				
+			image.set_color(color);
+			image.draw(canvas,src_rect,dest_rect);
+
+			offset_x = 33.0f;
+
+			dest_rect = Rectf(offset_x, offset_y, Sizef(31, 47));
+			src_rect = Rectf(0.25f*image_size.width,
+							 0.25f*image_size.width,	
+							 0.75f*image_size.height, 
+							 0.75f*image_size.height);
+			color = Colorf(1.0f, 0.0f, 0.0f, 1.0f);
+
+			image.set_color(color);
+			image.draw(canvas,src_rect,dest_rect);
+			
+			small_font.draw_text(canvas, 76, 260, "Image (0, 250) size=(31,47)");
+			small_font.draw_text(canvas, 76, 275, "Image (33, 250) size=(31,47) (red, magnify*2)");
 
 	// RoundedRect
 			{
@@ -264,10 +279,10 @@ int App::start(const std::vector<std::string> &args)
 				float offset_x = 0.0f;
 				float offset_y = 300.0f;
 				Origin origin = origin_top_left;
-				roundedrect.draw(gc, Pointf(offset_x, offset_y), Colorf(1.0f, 1.0f, 1.0f, 1.0f), origin);
+				roundedrect.draw(canvas, Pointf(offset_x, offset_y), Colorf(1.0f, 1.0f, 1.0f, 1.0f), origin);
 
 				offset_y = 340.0f;
-				roundedrect.fill(gc, Pointf(offset_x, offset_y), Colorf(1.0f, 1.0f, 1.0f, 1.0f), origin);
+				roundedrect.fill(canvas, Pointf(offset_x, offset_y), Colorf(1.0f, 1.0f, 1.0f, 1.0f), origin);
 
 				offset_y = 380.0f;
 				Gradient gradient;
@@ -275,7 +290,7 @@ int App::start(const std::vector<std::string> &args)
 				gradient.top_right = Colorf(1.0f, 0.0f, 0.0f, 1.0f);
 				gradient.bottom_left = Colorf(0.0f, 0.0f, 1.0f, 1.0f);
 				gradient.bottom_right = Colorf(0.0f, 1.0f, 0.0f, 1.0f);
-				roundedrect.fill(gc, Pointf(offset_x, offset_y), gradient, origin);
+				roundedrect.fill(canvas, Pointf(offset_x, offset_y), gradient, origin);
 
 				offset_y = 420.0f;
 				roundedrect.set_control_point_bl(Pointf(0.4f, 0.8f));
@@ -289,18 +304,18 @@ int App::start(const std::vector<std::string> &args)
 				roundedrect.set_control_point_br(Pointf(0.6f, 0.8f));
 				roundedrect.set_rounding_bottom_right(Sizef(0.8f, 0.6f));
 
-				roundedrect.fill(gc, Pointf(offset_x, offset_y), gradient, origin);
+				roundedrect.fill(canvas, Pointf(offset_x, offset_y), gradient, origin);
 
 			}
 
-			small_font.draw_text(gc, 76, 310, "RoundedRect - draw (0, 300) size=(64,32)");
-			small_font.draw_text(gc, 76, 325, "(RoundedRect - draw gradient - is not implemented)");
-			small_font.draw_text(gc, 76, 350, "RoundedRect - fill (0, 340) size=(64,32)");
-			small_font.draw_text(gc, 76, 390, "RoundedRect - fill gradient (0, 380) size=(64,32)");
-			small_font.draw_text(gc, 76, 400, "top left = white. top right = red");
-			small_font.draw_text(gc, 76, 410, "bottom left = blue. bottom right = green");
-			small_font.draw_text(gc, 76, 430, "RoundedRect - fill gradient (0, 420) size=(64,32)");
-			small_font.draw_text(gc, 76, 440, "Controling control / rounding points");
+			small_font.draw_text(canvas, 76, 310, "RoundedRect - draw (0, 300) size=(64,32)");
+			small_font.draw_text(canvas, 76, 325, "(RoundedRect - draw gradient - is not implemented)");
+			small_font.draw_text(canvas, 76, 350, "RoundedRect - fill (0, 340) size=(64,32)");
+			small_font.draw_text(canvas, 76, 390, "RoundedRect - fill gradient (0, 380) size=(64,32)");
+			small_font.draw_text(canvas, 76, 400, "top left = white. top right = red");
+			small_font.draw_text(canvas, 76, 410, "bottom left = blue. bottom right = green");
+			small_font.draw_text(canvas, 76, 430, "RoundedRect - fill gradient (0, 420) size=(64,32)");
+			small_font.draw_text(canvas, 76, 440, "Controling control / rounding points");
 
 	// Sprite
 			{
@@ -312,16 +327,16 @@ int App::start(const std::vector<std::string> &args)
 #ifndef USE_D3D
 				glEnable(GL_MULTISAMPLE);
 #endif
-				sprite.set_base_angle(Angle(test_base_angle, cl_degrees));
-				sprite.draw(gc, 350, 20);
-				sprite.set_base_angle(Angle(0, cl_degrees));
+				sprite.set_base_angle(Angle(test_base_angle, angle_degrees));
+				sprite.draw(canvas, 350, 20);
+				sprite.set_base_angle(Angle(0, angle_degrees));
 
 #ifndef USE_D3D
 				glDisable(GL_MULTISAMPLE);
 #endif
 			}
-			small_font.draw_text(gc, 370, 20, "Sprite - Base angle");
-			small_font.draw_text(gc, 370, 35, "Multisampling enabled");
+			small_font.draw_text(canvas, 370, 20, "Sprite - Base angle");
+			small_font.draw_text(canvas, 370, 35, "Multisampling enabled");
 
 			{
 				test_angle+=5.0f;
@@ -330,12 +345,16 @@ int App::start(const std::vector<std::string> &args)
 					test_angle = 0.0f;
 				}
 
-				sprite.set_angle(Angle(test_angle, cl_degrees));
-				sprite.draw(gc, 350, 60);
-				sprite.set_angle(Angle(0, cl_degrees));
+				sprite.set_angle(Angle(test_angle, angle_degrees));
+				sprite.draw(canvas, 350, 60);
+				sprite.set_angle(Angle(0, angle_degrees));
 			}
-			small_font.draw_text(gc, 370, 60, "Sprite - Angle");
+			small_font.draw_text(canvas, 370, 60, "Sprite - Angle");
+			
+				//Sprite yaw and pitch is not supported in ClanLib 3.0.
+				//The same effect can be achieved with the scaling feature.
 
+			/*
 			{
 				test_angle_pitch+=5.0f;
 				if (test_angle_pitch >= 360.0f)
@@ -343,12 +362,12 @@ int App::start(const std::vector<std::string> &args)
 					test_angle_pitch = 0.0f;
 				}
 
-				sprite.set_angle_pitch(Angle(test_angle_pitch, cl_degrees));
-				sprite.draw(gc, 350, 100);
-				sprite.set_angle_pitch(Angle(0, cl_degrees));
+				sprite.set_angle_pitch(Angle(test_angle_pitch, angle_degrees));
+				sprite.draw(canvas, 350, 100);
+				sprite.set_angle_pitch(Angle(0, angle_degrees));
 			}
-			small_font.draw_text(gc, 370, 100, "Sprite - Angle Pitch");
-
+			small_font.draw_text(canvas, 370, 100, "Sprite - Angle Pitch");
+			
 			{
 				test_angle_yaw+=5.0f;
 				if (test_angle_yaw >= 360.0f)
@@ -356,11 +375,12 @@ int App::start(const std::vector<std::string> &args)
 					test_angle_yaw = 0.0f;
 				}
 
-				sprite.set_angle_yaw(Angle(test_angle_yaw, cl_degrees));
-				sprite.draw(gc, 350, 140);
-				sprite.set_angle_yaw(Angle(0, cl_degrees));
+				sprite.set_angle_yaw(Angle(test_angle_yaw, angle_degrees));
+				sprite.draw(canvas, 350, 140);
+				sprite.set_angle_yaw(Angle(0, angle_degrees));
 			}
-			small_font.draw_text(gc, 370, 140, "Sprite - Angle Yaw");
+			small_font.draw_text(canvas, 370, 140, "Sprite - Angle Yaw");
+			*/
 
 			{
 				if (test_scale_dir)
@@ -380,14 +400,17 @@ int App::start(const std::vector<std::string> &args)
 						test_scale_dir = true;
 					}
 				}
+			small_font.draw_text(canvas, 420, 80, "Font - draw_text before scaling Sprite");
+			small_font.draw_text(canvas, 420, 100, "Sprite - Set Scale (x), (y)");
 
 				sprite.set_scale(test_scale, 1.0f);
-				sprite.draw(gc, 350, 180);
+				sprite.draw(canvas, 350, 100);
 				sprite.set_scale(1.0f, test_scale);
-				sprite.draw(gc, 390, 180);
+				sprite.draw(canvas, 390, 100);
 				sprite.set_scale(1.0f, 1.0f);
 			}
-			small_font.draw_text(gc, 420, 180, "Sprite - Set Scale (x), (y)");
+
+			small_font.draw_text(canvas, 420, 120, "Font - draw_text after scaling Sprite");
 
 			// Flip the display, showing on the screen what we have drawed
 			// since last call to flip()
@@ -425,7 +448,7 @@ int App::start(const std::vector<std::string> &args)
 // A key was pressed
 void App::on_input_up(const InputEvent &key)
 {
-	if(key.id == KEY_ESCAPE)
+	if(key.id == keycode_escape)
 	{
 		quit = true;
 	}
