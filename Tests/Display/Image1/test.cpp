cbb94175a2c8bf766f3cc7caca532fc20a265722
Griswoldz <Grizmu@gmail.com>
2012-11-06 21:57:44 +0000

    Updated Tests/Display/Image1, because it's cool, for the ClanLib 3.0

--- Tests/Display/Image1/test.cpp
+++ Tests/Display/Image1/test.cpp
@@ -24,18 +24,19 @@ public:
 		try
 		{
 			DisplayWindow window("Image test", 1024, 768);
-			GraphicContext gc = window.get_gc();
+
+			Canvas canvas(window);
+			GraphicContext &gc = canvas.get_gc();
 
 			// Connect the Window close event
 			Slot slot_quit = window.sig_window_close().connect(this, &App::on_window_close);
-
-			gc.enable_blending(true);
+			//gc.enable_blending(true);
 
 			quit = false;
 
 			ResourceManager resources("resources.xml");
 
-			Texture texture(gc, "Images/square.png");
+			Texture2D texture(gc, "Images/square.png");
 
 			Image image_texture(gc, texture, Rect(0, 0, texture.get_size()));
 			Image image_loaded(gc, "Images/square.png");
@@ -51,102 +52,102 @@ public:
 			//Console::write_line("Scale: %1,%2", image_resources.get_scale_x(), image_resources.get_scale_y());
 			//Console::write_line("Translation: %1,%2,%3", image_resources.get_alignment());
 
-			while((!quit) && (!window.get_ic().get_keyboard().get_keycode(KEY_ESCAPE)))
+			while((!quit) && (!window.get_ic().get_keyboard().get_keycode(keycode_escape)))
 			{
 				gc.clear(Colorf(0.5f,0.5f,0.5f));
 
-				small_font.draw_text(gc, 10, 40, "Image From Texture (10,60)");
-				image_texture.draw(gc, 10, 60);
+				small_font.draw_text(canvas, 10, 40, "Image From Texture (10,60)");
+				image_texture.draw(canvas, 10, 60);
 
-				small_font.draw_text(gc, 150, 40, "Image From Load (150,60)");
-				image_loaded.draw(gc, 150, 60);
+				small_font.draw_text(canvas, 150, 40, "Image From Load (150,60)");
+				image_loaded.draw(canvas, 150, 60);
 
-				small_font.draw_text(gc, 300, 40, "Image From Resources (300,60)");
-				image_resources.draw(gc, 300, 60);
+				small_font.draw_text(canvas, 300, 40, "Image From Resources (300,60)");
+				image_resources.draw(canvas, 300, 60);
 
-				small_font.draw_text(gc, 450, 40, "Image Copied (450,60)");
-				image_copy.draw(gc, 450, 60);
+				small_font.draw_text(canvas, 450, 40, "Image Copied (450,60)");
+				image_copy.draw(canvas, 450, 60);
 
-				small_font.draw_text(gc, 10, 190, "Image - Top Right (10,200)");
-				image_top_right.draw(gc, 10, 200);
+				small_font.draw_text(canvas, 10, 190, "Image - Top Right (10,200)");
+				image_top_right.draw(canvas, 10, 200);
 
-				small_font.draw_text(gc, 150, 190, "Image - Top Right (150,200)");
-				image_texture.draw(gc, Rect(32, 0, Size(32, 32)), Rect(150, 200, Size(32, 32)));
+				small_font.draw_text(canvas, 150, 190, "Image - Top Right (150,200)");
+				image_texture.draw(canvas, Rect(32, 0, Size(32, 32)), Rect(150, 200, Size(32, 32)));
 
-				small_font.draw_text(gc, 300, 190, "Image - Bottom Right (300,200)");
-				image_bottom_right.draw(gc, 300, 200);
+				small_font.draw_text(canvas, 300, 190, "Image - Bottom Right (300,200)");
+				image_bottom_right.draw(canvas, 300, 200);
 
-				small_font.draw_text(gc, 450, 190, "Image - Bottom Right (450,200)");
-				image_texture.draw(gc, Rect(32, 32, Size(32, 32)), Rect(450, 200, Size(32, 32)));
+				small_font.draw_text(canvas, 450, 190, "Image - Bottom Right (450,200)");
+				image_texture.draw(canvas, Rect(32, 32, Size(32, 32)), Rect(450, 200, Size(32, 32)));
 
-				small_font.draw_text(gc, 10, 290, "700 Images (10,300)");
+				small_font.draw_text(canvas, 10, 290, "700 Images (10,300)");
 				for(int i=0;i<700;i++)
-					image_texture.draw(gc, 10, 300);
+					image_texture.draw(canvas, 10, 300);
 
-				small_font.draw_text(gc, 150, 290, "br image (150,400) Size(128,256)");
-				image_bottom_right.draw(gc, Rect(150, 300, Size(128, 256)));
+				small_font.draw_text(canvas, 150, 290, "br image (150,400) Size(128,256)");
+				image_bottom_right.draw(canvas, Rect(150, 300, Size(128, 256)));
 
-				small_font.draw_text(gc, 300, 290, "Image - black");
-				image_black.draw(gc, 300, 300);
+				small_font.draw_text(canvas, 300, 290, "Image - black");
+				image_black.draw(canvas, 300, 300);
 
-				small_font.draw_text(gc, 300, 490, "Image - Scale (1.5, 2.5)");
+				small_font.draw_text(canvas, 300, 490, "Image - Scale (1.5, 2.5)");
 				image_texture.set_scale(1.5f, 2.5f);
-				image_texture.draw(gc, 300, 500);
+				image_texture.draw(canvas, 300, 500);
 				image_texture.set_scale(1.0f, 1.0f);
 
-				small_font.draw_text(gc, 450, 460, "Image - Alignment (4 images with 8 pixel offset)");
-				small_font.draw_text(gc, 450, 475, "(top left, top right, bottom left, bottom right)");
-				small_font.draw_text(gc, 450, 490, "(Circle denotes the draw origin)");
+				small_font.draw_text(canvas, 450, 460, "Image - Alignment (4 images with 8 pixel offset)");
+				small_font.draw_text(canvas, 450, 475, "(top left, top right, bottom left, bottom right)");
+				small_font.draw_text(canvas, 450, 490, "(Circle denotes the draw origin)");
 				const int offset = 96;
 
 				image_texture.set_alignment(origin_top_left, 8, 8);
-				image_texture.draw(gc, 450+offset, 500+offset);
+				image_texture.draw(canvas, 450+offset, 500+offset);
 				image_texture.set_alignment(origin_top_right, -8, 8);
-				image_texture.draw(gc, 450+offset, 500+offset);
+				image_texture.draw(canvas, 450+offset, 500+offset);
 
 				image_texture.set_alignment(origin_bottom_left, 8, -8);
-				image_texture.draw(gc, 450+offset, 500+offset);
+				image_texture.draw(canvas, 450+offset, 500+offset);
 				image_texture.set_alignment(origin_bottom_right, -8, -8);
-				image_texture.draw(gc, 450+offset, 500+offset);
+				image_texture.draw(canvas, 450+offset, 500+offset);
 
-				Draw::circle(gc, 450+offset, 500+offset, 4, Colorf(1.0f, 1.0f, 1.0f, 0.9f));
+				canvas.circle(450+offset, 500+offset, 4, Colorf(1.0f, 1.0f, 1.0f, 0.9f));
 
-				small_font.draw_text(gc, 700, 460, "Image - Center Alignment (4 images with 8 pixel offset)");
-				small_font.draw_text(gc, 700, 475, "(top center, right center, bottom center, left center)");
-				small_font.draw_text(gc, 700, 490, "(Circle denotes the draw origin)");
-
+				small_font.draw_text(canvas, 700, 460, "Image - Center Alignment (4 images with 8 pixel offset)");
+				small_font.draw_text(canvas, 700, 475, "(top center, right center, bottom center, left center)");
+				small_font.draw_text(canvas, 700, 490, "(Circle denotes the draw origin)");
+				
 				image_texture.set_alignment(origin_top_center, 0, 8);
-				image_texture.draw(gc, 700+offset, 500+offset);
+				image_texture.draw(canvas, 700+offset, 500+offset);
 				image_texture.set_alignment(origin_bottom_center, 0, -8);
-				image_texture.draw(gc, 700+offset, 500+offset);
+				image_texture.draw(canvas, 700+offset, 500+offset);
 
 				image_texture.set_alignment(origin_center_left, 8, 0);
-				image_texture.draw(gc, 700+offset, 500+offset);
+				image_texture.draw(canvas, 700+offset, 500+offset);
 				image_texture.set_alignment(origin_center_right, -8, 0);
-				image_texture.draw(gc, 700+offset, 500+offset);
+				image_texture.draw(canvas, 700+offset, 500+offset);
 
-				Draw::circle(gc, 700+offset, 500+offset, 4, Colorf(1.0f, 1.0f, 1.0f, 0.9f));
+				canvas.circle(700+offset, 500+offset, 4, Colorf(1.0f, 1.0f, 1.0f, 0.9f));
 
-				small_font.draw_text(gc, 700, 160, "Image - Center Align (4 images with 64 pixel offset)");
-				small_font.draw_text(gc, 700, 175, "Also Includes a centered image (Without offset)");
-				small_font.draw_text(gc, 700, 190, "(Circle denotes the draw origin)");
+				small_font.draw_text(canvas, 700, 160, "Image - Center Align (4 images with 64 pixel offset)");
+				small_font.draw_text(canvas, 700, 175, "Also Includes a centered image (Without offset)");
+				small_font.draw_text(canvas, 700, 190, "(Circle denotes the draw origin)");
 
 				const int center_image_offset = 64;
 
 				image_texture.set_alignment(origin_center, 0, 0);
-				image_texture.draw(gc, 700+offset, 200+offset);
+				image_texture.draw(canvas, 700+offset, 200+offset);
 
 				image_texture.set_alignment(origin_center, 0, center_image_offset);
-				image_texture.draw(gc, 700+offset, 200+offset);
+				image_texture.draw(canvas, 700+offset, 200+offset);
 				image_texture.set_alignment(origin_center, 0, -center_image_offset);
-				image_texture.draw(gc, 700+offset, 200+offset);
+				image_texture.draw(canvas, 700+offset, 200+offset);
 
 				image_texture.set_alignment(origin_center, center_image_offset, 0);
-				image_texture.draw(gc, 700+offset, 200+offset);
+				image_texture.draw(canvas, 700+offset, 200+offset);
 				image_texture.set_alignment(origin_center, -center_image_offset, 0);
-				image_texture.draw(gc, 700+offset, 200+offset);
+				image_texture.draw(canvas, 700+offset, 200+offset);
 
-				Draw::circle(gc, 700+offset, 200+offset, 4, Colorf(1.0f, 1.0f, 1.0f, 0.9f));
+				canvas.circle(700+offset, 200+offset, 4, Colorf(1.0f, 1.0f, 1.0f, 0.9f));
 
 				// Restore alignment
 				image_texture.set_alignment(origin_top_left, 0, 0);
