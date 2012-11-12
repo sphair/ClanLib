2e9c38547a93adb21e8df21d5471af5961883102
Kenneth Gangstoe <sphair@gmail.com>
2012-11-12 10:47:49 +0000

    - added some imageviwe scaling/fit tests

--- Tests/GUI/CSSDocument2/program.cpp
+++ Tests/GUI/CSSDocument2/program.cpp
@@ -62,14 +62,37 @@ int Program::main(const std::vector<std::string> &args)
 	testHot2->set_class("test2");
 	GUIComponent *testHot3 = new GUIComponent(testHot2);
 	testHot3->set_class("test3");
-
-	ImageView *image_view = new ImageView(root);
+	
 	Image image(root->get_canvas(), "../../../Examples/GUI/CommonCode/Resources/tux.png");
-	image_view->set_image(image);
 
-   
 	Label *label1 = new Label(root);
 	label1->set_text("Hello World");
+
+	// Preferred size
+	ImageView *image_view = new ImageView(root);
+	image_view->set_image(image);
+	image_view->set_class("normal");
+
+	// Larger fixed width than preferred content
+	ImageView *image_view2 = new ImageView(root);
+	image_view2->set_image(image);
+	image_view2->set_class("fixed");
+   
+	// 2x scale in preferred size
+	ImageView *image_view3 = new ImageView(root);
+	image_view3->set_image(image);
+	image_view3->set_scale(2.0f, 2.0f);
+
+	// Scale to fit in preferred size
+	ImageView *image_view4 = new ImageView(root);
+	image_view4->set_image(image);
+	image_view4->set_scale_to_fit();
+
+	// Scale to fit in larger fixed width than preferred content
+	ImageView *image_view5 = new ImageView(root);
+	image_view5->set_image(image);
+	image_view5->set_scale_to_fit();
+	image_view5->set_class("fixed");
 
 	root->update_layout();
 
