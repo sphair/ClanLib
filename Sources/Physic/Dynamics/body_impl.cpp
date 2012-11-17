90e5b29efadaca7c1e987fce10cc73f12efcc43f
Griswoldz <Grizmu@gmail.com>
2012-11-17 14:05:42 +0000

    Box2D Physics exclusively for ClanLib 3.0!

---
+++ Sources/Physic/Dynamics/body_impl.cpp
@@ -0,0 +1,65 @@
+/*
+**  ClanLib SDK
+**  Copyright (c) 1997-2012 The ClanLib Team
+**
+**  This software is provided 'as-is', without any express or implied
+**  warranty.  In no event will the authors be held liable for any damages
+**  arising from the use of this software.
+**
+**  Permission is granted to anyone to use this software for any purpose,
+**  including commercial applications, and to alter it and redistribute it
+**  freely, subject to the following restrictions:
+**
+**  1. The origin of this software must not be misrepresented; you must not
+**     claim that you wrote the original software. If you use this software
+**     in a product, an acknowledgment in the product documentation would be
+**     appreciated but is not required.
+**  2. Altered source versions must be plainly marked as such, and must not be
+**     misrepresented as being the original software.
+**  3. This notice may not be removed or altered from any source distribution.
+**
+**  Note: Some of the libraries ClanLib may link to may have additional
+**  requirements or restrictions.
+**
+**  File Author(s):
+**
+**    Arkadiusz Kalinowski
+*/
+
+#include "Physic/precomp.h"
+#include "Box2D/Box2D.h"
+#include "body_impl.h"
+#include "body_description_impl.h"
+#include "../World/physic_world_impl.h"
+
+namespace clan
+{
+
+Body_Impl::Body_Impl()
+{
+	body = NULL;
+	body_occupied = false;
+}
+
+void Body_Impl::create_body(PhysicWorld &pw, const BodyDescription &description)
+{
+	
+	if(body_occupied)	//Add proper handling of physic world in a case of a deletion
+	{
+		body->GetWorld()->DestroyBody(body);
+	}
+	else
+	{
+		body_occupied = true;
+	}
+
+	body = pw.impl->create_body(description.impl->bodyDef);
+		
+}
+
+b2Fixture *Body_Impl::create_fixture(b2FixtureDef &description) // obsolete
+{
+	return body->CreateFixture(&description);
+}
+
+}
