55c64822150f2bf145bddc7ecd280882a965618e
Griswoldz <Grizmu@gmail.com>
2012-12-05 09:06:13 +0000

    Added Physics/Collision test.

---
+++ Tests/Physics/Collision/Sources/precomp.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <ClanLib/core.h>
+#include <ClanLib/application.h>
+#include <ClanLib/display.h>
+#include <ClanLib/physics.h>
+#include <memory>
+
+#include <cmath>
+
+using namespace clan;
+
