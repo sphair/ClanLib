1e1cf08404b10d84f0c54df69b0ee1da7b43c73a
rombust <rombust@hotmail.co.uk>
2012-12-06 22:04:44 +0000

    Update the CSSDocument2 test, now displaying the RadioButton, CheckBox, Slider, ScrollBar and LineEdit

--- Tests/GUI/CSSDocument2/precomp.h
+++ Tests/GUI/CSSDocument2/precomp.h
@@ -5,4 +5,5 @@
 #include <ClanLib/core.h>
 #include <ClanLib/display.h>
 #include <ClanLib/gl.h>
+#include <ClanLib/gl1.h>
 #include <ClanLib/gui.h>
--- Tests/GUI/CSSDocument2/program.cpp
+++ Tests/GUI/CSSDocument2/program.cpp
@@ -156,50 +156,50 @@ void Program::create_component(DomElement xml_element, GUIComponent *parent)
 		{
 		}
 	}
-	//else if (xml_element.get_tag_name() == "radiobutton")
-	//{
-	//	RadioButton *component = new RadioButton(parent);
-	//	component->set_class(xml_element.get_attribute("class"));
-	//	std::vector<std::string> pseudo_classes = StringHelp::split_text(xml_element.get_attribute("pseudo-class"), " ");
-	//	for (size_t i = 0; i < pseudo_classes.size(); i++)
-	//		component->set_pseudo_class(pseudo_classes[i], true);
+	else if (xml_element.get_tag_name() == "radiobutton")
+	{
+		RadioButton *component = new RadioButton(parent);
+		component->set_class(xml_element.get_attribute("class"));
+		std::vector<std::string> pseudo_classes = StringHelp::split_text(xml_element.get_attribute("pseudo-class"), " ");
+		for (size_t i = 0; i < pseudo_classes.size(); i++)
+			component->set_pseudo_class(pseudo_classes[i], true);
 
-	//	try
-	//	{
-	//		std::string text = xml_element.select_string("text/text()");
-	//		component->set_text(text);
-	//	}
-	//	catch (Exception &)
-	//	{
-	//	}
+		try
+		{
+			std::string text = xml_element.select_string("text/text()");
+			component->set_text(text);
+		}
+		catch (Exception &)
+		{
+		}
 
-	//	try
-	//	{
-	//		std::string group = xml_element.select_string("group/text()");
-	//		component->set_group_name(group);
-	//	}
-	//	catch (Exception &)
-	//	{
-	//	}
-	//}
-	//else if (xml_element.get_tag_name() == "checkbox")
-	//{
-	//	CheckBox *component = new CheckBox(parent);
-	//	component->set_class(xml_element.get_attribute("class"));
-	//	std::vector<std::string> pseudo_classes = StringHelp::split_text(xml_element.get_attribute("pseudo-class"), " ");
-	//	for (size_t i = 0; i < pseudo_classes.size(); i++)
-	//		component->set_pseudo_class(pseudo_classes[i], true);
-	//	component->set_text(xml_element.get_text());
-	//}		
-	//else if (xml_element.get_tag_name() == "lineedit")
-	//{
-	//	LineEdit *component = new LineEdit(parent);
-	//	component->set_class(xml_element.get_attribute("class"));
-	//	std::vector<std::string> pseudo_classes = StringHelp::split_text(xml_element.get_attribute("pseudo-class"), " ");
-	//	for (size_t i = 0; i < pseudo_classes.size(); i++)
-	//		component->set_pseudo_class(pseudo_classes[i], true);
-	//	component->set_text(xml_element.get_text());
-	//}	
+		try
+		{
+			std::string group = xml_element.select_string("group/text()");
+			component->set_group_name(group);
+		}
+		catch (Exception &)
+		{
+		}
+	}
+	else if (xml_element.get_tag_name() == "checkbox")
+	{
+		CheckBox *component = new CheckBox(parent);
+		component->set_class(xml_element.get_attribute("class"));
+		std::vector<std::string> pseudo_classes = StringHelp::split_text(xml_element.get_attribute("pseudo-class"), " ");
+		for (size_t i = 0; i < pseudo_classes.size(); i++)
+			component->set_pseudo_class(pseudo_classes[i], true);
+		component->set_text(xml_element.get_text());
+	}		
+	else if (xml_element.get_tag_name() == "lineedit")
+	{
+		LineEdit *component = new LineEdit(parent);
+		component->set_class(xml_element.get_attribute("class"));
+		std::vector<std::string> pseudo_classes = StringHelp::split_text(xml_element.get_attribute("pseudo-class"), " ");
+		for (size_t i = 0; i < pseudo_classes.size(); i++)
+			component->set_pseudo_class(pseudo_classes[i], true);
+		component->set_text(xml_element.get_text());
+	}	
 
 	else if (xml_element.get_tag_name() == "scrollbar")
 	{
@@ -212,17 +212,17 @@ void Program::create_component(DomElement xml_element, GUIComponent *parent)
 		component->set_ranges(100, 200, 5, 20);
 	}
 
-	//else if (xml_element.get_tag_name() == "slider")
-	//{
-	//	Slider *component = new Slider(parent);
-	//	component->set_class(xml_element.get_attribute("class"));
-	//	std::vector<std::string> pseudo_classes = StringHelp::split_text(xml_element.get_attribute("pseudo-class"), " ");
-	//	for (size_t i = 0; i < pseudo_classes.size(); i++)
-	//		component->set_pseudo_class(pseudo_classes[i], true);
+	else if (xml_element.get_tag_name() == "slider")
+	{
+		Slider *component = new Slider(parent);
+		component->set_class(xml_element.get_attribute("class"));
+		std::vector<std::string> pseudo_classes = StringHelp::split_text(xml_element.get_attribute("pseudo-class"), " ");
+		for (size_t i = 0; i < pseudo_classes.size(); i++)
+			component->set_pseudo_class(pseudo_classes[i], true);
 
-	//	component->set_ranges(100, 200, 5, 20);
+		component->set_ranges(100, 200, 5, 20);
 
-	//}	
+	}	
 	else
 	{
 		GUIComponent *component = new GUIComponent(parent, xml_element.get_tag_name());
