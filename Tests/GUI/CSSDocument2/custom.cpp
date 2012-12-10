303eb3552115bcb915cf72a7786eaef437858dd5
rombust <rombust@hotmail.co.uk>
2012-12-10 08:03:49 +0000

    Update the CSSDocument2 test, wondering why render_box() is not lining up to the parent content box

--- Tests/GUI/CSSDocument2/custom.cpp
+++ Tests/GUI/CSSDocument2/custom.cpp
@@ -25,10 +25,10 @@ float Custom::get_preferred_content_height(float width)
 
 void Custom::on_render(Canvas &canvas, const Rect &update_rect)
 {
-	Rect rect = get_content_box();
+	Rect content_rect = get_content_box();
 
-	Rect box1_rect(0, 0, Size(box1.get_preferred_width(), 20));
-	Rect box2_rect(box1_rect.right, 0, Size(box2.get_preferred_width(), 20));
+	Rect box1_rect(content_rect.left, content_rect.top, Size(box1.get_preferred_width(), 20));
+	Rect box2_rect(box1_rect.right, box1_rect.top, Size(box2.get_preferred_width(), 20));
 
 	box1.render_box(canvas, box1_rect);
 	box2.render_box(canvas, box2_rect);
