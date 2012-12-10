08038c0272bd8bafcab6e9b61f7ab0231448be1f
rombust <rombust@hotmail.co.uk>
2012-12-10 09:58:45 +0000

    Adjust the CSSDocument2 render text in the custom component to use the content box

--- Tests/GUI/CSSDocument2/custom.cpp
+++ Tests/GUI/CSSDocument2/custom.cpp
@@ -33,7 +33,7 @@ void Custom::on_render(Canvas &canvas, const Rect &update_rect)
 	box1.render_box(canvas, box1_rect);
 	box2.render_box(canvas, box2_rect);
 
-	box1.render_text(canvas, "1", box1_rect);
-	box2.render_text(canvas, "2", box2_rect);
+	box1.render_text(canvas, "1", box1.get_content_box(box1_rect));
+	box2.render_text(canvas, "2", box2.get_content_box(box2_rect));
 
 }
