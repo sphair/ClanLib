04eebfd0c346c9b1047aa19f066e2759b27c5cba
Griswoldz <Grizmu@gmail.com>
2012-12-12 18:05:04 +0000

    Added Physics/Raycasting test.

---
+++ Tests/Physics/Raycasting/Sources/precomp.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <ClanLib/core.h>
+#include <ClanLib/application.h>
+#include <ClanLib/display.h>
+#include <ClanLib/physics.h>
+
+#include <cmath>
+
+using namespace clan;
+
