559eed0790ae17bb69127db2d4c9c482016a896e
rombust <rombust@hotmail.co.uk>
2012-12-12 09:24:06 +0000

    Add the margin test to CSSDocument2 Custom component example

--- Tests/GUI/CSSDocument2/custom.cpp
+++ Tests/GUI/CSSDocument2/custom.cpp
@@ -36,4 +36,8 @@ void Custom::on_render(Canvas &canvas, const Rect &update_rect)
 	box1.render_text(canvas, "1", box1.get_content_box(box1_rect));
 	box2.render_text(canvas, "2", box2.get_content_box(box2_rect));
 
+	Rect component_rect = get_geometry();
+
+
+
 }
