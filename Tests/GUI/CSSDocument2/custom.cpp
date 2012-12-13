95d5c6eb554b8ecbf047cc4a579213a0f97fabb5
rombust <rombust@hotmail.co.uk>
2012-12-13 16:19:18 +0000

    Extend the CSSDocument2 custom component, ready to test font alignment (when implemented)

--- Tests/GUI/CSSDocument2/custom.cpp
+++ Tests/GUI/CSSDocument2/custom.cpp
@@ -15,7 +15,7 @@ Custom::Custom(GUIComponent *parent)
 
 float Custom::get_preferred_content_width()
 {
-	return 100.0f;
+	return 800.0f;
 }
 
 float Custom::get_preferred_content_height(float width)
@@ -27,14 +27,27 @@ void Custom::on_render(Canvas &canvas, const Rect &update_rect)
 {
 	Rect content_rect = get_content_box();
 
-	Rect box1_rect(content_rect.left, content_rect.top, Size(box1.get_css_width(), 40));
-	Rect box2_rect(box1_rect.right, box1_rect.top, Size(box2.get_css_width(), 40));
+	Rect box1_rect(content_rect.left, content_rect.top, box1.get_css_size());
+	Rect box2_rect(box1_rect.right, box1_rect.top, box2.get_css_size());
 
 	box1.render_box(canvas, box1_rect);
 	box2.render_box(canvas, box2_rect);
 
-	box1.render_text(canvas, "1", box1.get_content_box(box1_rect));
-	box2.render_text(canvas, "2", box2.get_content_box(box2_rect));
+	static char message_1[] = "gratulerer med dagen";
+	static char message_2[] = "rød grønn blå";
+
+	Rect text1_rect = box1.get_render_text_box(canvas, message_1);
+	Rect text2_rect = box2.get_render_text_box(canvas, message_2);
+
+	Rect box1_content_box = box1.get_content_box(box1_rect);
+	Rect box2_content_box = box2.get_content_box(box2_rect);
+
+	canvas.fill(Rectf(box1_content_box.left, box1_content_box.top, Sizef(text1_rect.get_width(), text1_rect.get_height())), Colorf(0.5f, 0.5f, 0.5f, 0.5f));
+	canvas.fill(Rectf(box2_content_box.left, box2_content_box.top, Sizef(text2_rect.get_width(), text2_rect.get_height())), Colorf(0.5f, 0.5f, 0.5f, 0.5f));
+
+	box1.render_text(canvas, message_1, box1_content_box);
+	box2.render_text(canvas, message_2, box2_content_box);
+
 
 	Rect component_rect = get_geometry();
 
