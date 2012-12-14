3ead17e2e34ae14b2cb89ab07a570fcc345ad616
rombust <rombust@hotmail.co.uk>
2012-12-14 07:58:31 +0000

    Add a render_text_span test for the CSSDocument2 example, showing a minor fault in it's content box calculation (and justify alignment)

--- Tests/GUI/CSSDocument2/custom.cpp
+++ Tests/GUI/CSSDocument2/custom.cpp
@@ -11,11 +11,12 @@ Custom::Custom(GUIComponent *parent)
 
 	box1 = GUIThemePart(this, "box1");
 	box2 = GUIThemePart(this, "box2");
+	box3 = GUIThemePart(this, "box3");
 }
 
 float Custom::get_preferred_content_width()
 {
-	return 800.0f;
+	return 900.0f;
 }
 
 float Custom::get_preferred_content_height(float width)
@@ -29,24 +30,35 @@ void Custom::on_render(Canvas &canvas, const Rect &update_rect)
 
 	Rect box1_rect(content_rect.left, content_rect.top, box1.get_css_size());
 	Rect box2_rect(box1_rect.right, box1_rect.top, box2.get_css_size());
+	Rect box3_rect(box2_rect.right, box2_rect.top, box3.get_css_size());
 
 	box1.render_box(canvas, box1_rect);
 	box2.render_box(canvas, box2_rect);
+	box3.render_box(canvas, box3_rect);
 
 	static char message_1[] = "gratulerer med dagen";
 	static char message_2[] = "rød grønn blå";
+	static char message_3[] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet orci et eros lobortis accumsan id sed nulla. Curabitur bibendum lobortis ante ac fringilla";
 
 	Rect box1_content_box = box1.get_content_box(box1_rect);
 	Rect box2_content_box = box2.get_content_box(box2_rect);
+	Rect box3_content_box = box2.get_content_box(box3_rect);
 
 	// Note, get_render_text_size() exists, just does not require a content box for the theme part
 	Rect text1_rect = box1.get_render_text_box(canvas, message_1, box1_content_box);
 	Rect text2_rect = box2.get_render_text_box(canvas, message_2, box2_content_box);
+	Rect text3_rect = box2.get_render_text_span_box(canvas, message_3, box3_content_box);
 
 	canvas.fill(text1_rect, Colorf(0.5f, 0.5f, 0.5f, 0.5f));
 	canvas.fill(text2_rect, Colorf(0.5f, 0.5f, 0.5f, 0.5f));
+	canvas.fill(text3_rect, Colorf(0.5f, 0.5f, 0.5f, 0.5f));
 	
-	box1.render_text(canvas, message_1, box1_content_box);
-	box2.render_text(canvas, message_2, box2_content_box);
+	Rect rendered_text1_rect = box1.render_text(canvas, message_1, box1_content_box);
+	Rect rendered_text2_rect = box2.render_text(canvas, message_2, box2_content_box);
+	Rect rendered_text3_rect = box3.render_text_span(canvas, message_3, box3_content_box);
+
+	canvas.box(rendered_text1_rect, Colorf(0.5f, 0.5f, 0.5f, 1.0f));
+	canvas.box(rendered_text2_rect, Colorf(0.5f, 0.5f, 0.5f, 1.0f));
+	canvas.box(rendered_text3_rect, Colorf(0.5f, 0.5f, 0.5f, 1.0f));
 
 }
