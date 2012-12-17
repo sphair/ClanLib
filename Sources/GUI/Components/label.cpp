90ee8dc78a14bdf717d61d949577a709fab47da7
rombust <rombust@hotmail.co.uk>
2012-12-17 12:09:44 +0000

    Adjust lineedit, checkbox, radiobutton and label to all use get_vertical_text_align(). Doesn't work for an unknown reason

--- Sources/GUI/Components/label.cpp
+++ Sources/GUI/Components/label.cpp
@@ -152,7 +152,7 @@ void Label::set_alignment(Label::Alignment alignment)
 
 void Label_Impl::on_render(Canvas &canvas, const Rect &update_rect)
 {
-	label->render_text(canvas, text);
+	label->render_text(canvas, text, 0, label->get_vertical_text_align(canvas).baseline);
 }
 
 void Label_Impl::on_apply_properties(CSSBoxProperties &properties)
