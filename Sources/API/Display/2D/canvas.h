45045431159b22f2c76b288cefa3698c5160e072
rombust <rombust@hotmail.co.uk>
2013-01-17 10:39:52 +0000

    Fix draw_triangles typo, you don't need to pass the canvas into the canvas class

--- Sources/API/Display/2D/canvas.h
+++ Sources/API/Display/2D/canvas.h
@@ -418,7 +418,7 @@ public:
 	void draw_triangle(const Trianglef &dest_triangle, const Colorf &color = Colorf::white);
 
 	/// \brief Draw triangles
-	void draw_triangles(Canvas &canvas, Vec2f *triangle_positions, int num_vertices, const Colorf &color = Colorf::white);
+	void draw_triangles(Vec2f *triangle_positions, int num_vertices, const Colorf &color = Colorf::white);
 
 	/// \brief Draw a filled ellipse.
 	void draw_ellipse(const Pointf &center, float radius_x, float radius_y, const Colorf &color = Colorf::white);
