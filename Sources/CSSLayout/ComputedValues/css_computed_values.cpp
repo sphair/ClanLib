941319f6d9b3709498ba3c24fadee54dd22e9c48
mbn <mbn@cc39f7f4-b520-0410-a30f-b56705a9c917>
2013-01-19 17:25:40 +0000

    - Added class for computed values

---
+++ Sources/CSSLayout/ComputedValues/css_computed_values.cpp
@@ -0,0 +1,68 @@
+/*
+**  ClanLib SDK
+**  Copyright (c) 1997-2012 The ClanLib Team
+**
+**  This software is provided 'as-is', without any express or implied
+**  warranty.  In no event will the authors be held liable for any damages
+**  arising from the use of this software.
+**
+**  Permission is granted to anyone to use this software for any purpose,
+**  including commercial applications, and to alter it and redistribute it
+**  freely, subject to the following restrictions:
+**
+**  1. The origin of this software must not be misrepresented; you must not
+**     claim that you wrote the original software. If you use this software
+**     in a product, an acknowledgment in the product documentation would be
+**     appreciated but is not required.
+**  2. Altered source versions must be plainly marked as such, and must not be
+**     misrepresented as being the original software.
+**  3. This notice may not be removed or altered from any source distribution.
+**
+**  Note: Some of the libraries ClanLib may link to may have additional
+**  requirements or restrictions.
+**
+**  File Author(s):
+**
+**    Magnus Norddahl
+*/
+
+#include "CSSLayout/precomp.h"
+#include "API/CSSLayout/ComputedValues/css_computed_values.h"
+#include "css_computed_values_impl.h"
+
+namespace clan
+{
+
+CSSComputedValues::CSSComputedValues()
+{
+}
+
+CSSComputedValues::CSSComputedValues(const CSSComputedValues &parent)
+: impl(new CSSComputedValues_Impl())
+{
+}
+
+bool CSSComputedValues::is_null() const
+{
+	return !impl;
+}
+
+void CSSComputedValues::set_parent(const CSSComputedValues &parent)
+{
+}
+
+void CSSComputedValues::set_values(const std::vector<CSSPropertyValue *> &values)
+{
+}
+
+const CSSComputedBox *CSSComputedValues::get_box()
+{
+	return nullptr;
+}
+
+int CSSComputedValues::get_box_generation()
+{
+	return 0;
+}
+
+}
