c4a5918b38e0ad1ab302ac47b049b1875fa7083a
Kenneth Gangstoe <sphair@gmail.com>
2013-01-22 22:10:29 +0000

    TransferBuffer fixed for linux

--- Sources/Display/2D/render_batch_point.cpp
+++ Sources/Display/2D/render_batch_point.cpp
@@ -80,7 +80,9 @@ void RenderBatchPoint::lock_transfer_buffer(Canvas &canvas)
 	if (vertices == 0)
 	{
 		GraphicContext &gc = canvas.get_gc();
-		transfer_buffers = TransferVector<PointVertex>(batch_buffer->get_transfer_buffer(gc));
+
+		TransferBuffer transfer_buffer = batch_buffer->get_transfer_buffer(gc);
+		transfer_buffers = TransferVector<PointVertex>(transfer_buffer);
 		transfer_buffers.lock(gc, access_write_discard);
 		vertices = transfer_buffers.get_data();
 	}
