f0bb319a448fd972045af3ff79c4699c30253623
rombust <rombust@hotmail.co.uk>
2013-01-30 13:35:03 +0000

    Fix the textfade example

--- Examples/Display_Text/TextFade/Sources/textfade.cpp
+++ Examples/Display_Text/TextFade/Sources/textfade.cpp
@@ -36,58 +36,58 @@ int TextFade::start(const std::vector<std::string> &args)
 	quit = false;
 
 	// Set the window
-	DisplayWindowDescription desc;
+	clan::DisplayWindowDescription desc;
 	desc.set_title("ClanLib TextFade Example");
-	desc.set_size(Size(640, 480), true);
+	desc.set_size(clan::Size(640, 480), true);
 	desc.set_allow_resize(true);
 
-	DisplayWindow window(desc);
+	clan::DisplayWindow window(desc);
 
 	// Connect the Window close event
-	Slot slot_quit = window.sig_window_close().connect(this, &TextFade::on_window_close);
+	clan::Slot slot_quit = window.sig_window_close().connect(this, &TextFade::on_window_close);
 
 	// Connect a keyboard handler to on_key_up()
-	Slot slot_input_up = (window.get_ic().get_keyboard()).sig_key_up().connect(this, &TextFade::on_input_up);
+	clan::Slot slot_input_up = (window.get_ic().get_keyboard()).sig_key_up().connect(this, &TextFade::on_input_up);
 
 	// Get the graphic context
-	GraphicContext gc = window.get_gc();
+	clan::Canvas canvas(window);
 
-	Image background = Image(gc, "../../Game/DiceWar/Resources/lobby_background2.png");
+	clan::Image background(canvas, "../../Game/DiceWar/Resources/lobby_background2.png");
 
-	FontDescription font_description;
+	clan::FontDescription font_description;
 	font_description.set_typeface_name("arial");
 	font_description.set_height(48);
 	font_description.set_subpixel(false);	// Fading only works with sub pixel off
-	Font standard_font(gc, font_description);
+	clan::Font standard_font(canvas, font_description);
 
-	ResourceManager app_resources("../Font/Resources/resources.xml");
-	Font_Sprite sprite_font(gc, "ClanFont", &app_resources);
+	clan::ResourceManager app_resources("../Font/Resources/resources.xml");
+	clan::Font_Sprite sprite_font(canvas, "ClanFont", &app_resources);
 
 	// Run until someone presses escape
 	while (!quit)
 	{
-		background.draw(gc, gc.get_size());
+		background.draw(canvas, canvas.get_size());
 
-		draw_text(gc, standard_font, 100, "ABCDEFGHIJKLMNOPQRSTUVWXZ");
+		draw_text(canvas, standard_font, 100, "ABCDEFGHIJKLMNOPQRSTUVWXZ");
 
-		draw_text(gc, sprite_font, 300, "ABCDEFGHIJKLMNOPQRSTUVWXZ");
+		draw_text(canvas, sprite_font, 300, "ABCDEFGHIJKLMNOPQRSTUVWXZ");
 
-		window.flip(1);
+		canvas.flip(1);
 
 		// This call processes user input and other events
-		KeepAlive::process(0);
+		clan::KeepAlive::process(0);
 	}
 
 	return 0;
 }
 
-void TextFade::draw_text(GraphicContext &gc, Font &font, int ypos, const char *text)
+void TextFade::draw_text(clan::Canvas &canvas, clan::Font &font, int ypos, const char *text)
 {
 	// For this example, to keep things simple, we only handle ASCII characters
 
-	int gc_width = gc.get_width();
+	int gc_width = canvas.get_width();
 
-	Size text_size = font.get_text_size(gc, text);
+	clan::Size text_size = font.get_text_size(canvas, text);
 
 	int xpos = (gc_width - text_size.width) / 2;
 
@@ -104,17 +104,17 @@ void TextFade::draw_text(GraphicContext &gc, Font &font, int ypos, const char *t
 
 		float alpha = 1.0f - ( (float) position_off_centre * 2.0f / gc_width );
 		if (alpha > 0.0f)
-			font.draw_text(gc, xpos, ypos,  buffer, Colorf(0.4f, 0.4f, 1.0f, alpha));
-		xpos += font.get_glyph_size(gc, let).width;
+			font.draw_text(canvas, xpos, ypos,  buffer, clan::Colorf(0.4f, 0.4f, 1.0f, alpha));
+		xpos += font.get_glyph_size(canvas, let).width;
 	}
 
 }
 
 
 // A key was pressed
-void TextFade::on_input_up(const InputEvent &key)
+void TextFade::on_input_up(const clan::InputEvent &key)
 {
-	if(key.id == KEY_ESCAPE)
+	if(key.id == clan::keycode_escape)
 	{
 		quit = true;
 	}
