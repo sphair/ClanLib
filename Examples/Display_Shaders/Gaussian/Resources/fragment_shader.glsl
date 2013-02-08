597f204ae5ce90657bf1bfe9241a44b8655a8a20
rombust <rombust@hotmail.co.uk>
2013-02-08 08:21:06 +0000

    Tidy the Gaussian example (simplifying the shader)

--- Examples/Display_Shaders/Gaussian/Resources/fragment_shader.glsl
+++ Examples/Display_Shaders/Gaussian/Resources/fragment_shader.glsl
@@ -7,8 +7,7 @@ uniform sampler2D SourceTexture;
 layout (std140) uniform ProgramUniforms
 {
 	mat4 cl_ModelViewProjectionMatrix;
-	vec2 SampleOffsets[15];
-	float SampleWeights[15];
+	vec4 Sample[15];	// x,y = Offset. z = Weight
 };
 
 vec4 GaussianBlur()
@@ -16,7 +15,7 @@ vec4 GaussianBlur()
 	vec4 c = vec4(0.0);
 	for (int i = 0 ; i < 15; i++)
 	{
-		c += texture2D(SourceTexture, (TexCoord + SampleOffsets[i])) * SampleWeights[i];
+		c += texture2D(SourceTexture, (TexCoord + Sample[i].xy)) * Sample[i].z;
 	}
 
 	return c;
