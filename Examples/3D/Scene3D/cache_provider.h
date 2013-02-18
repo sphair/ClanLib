1238ee631d8aae1f3b4a02482cf9ee477c089c38
mbn <mbn@cc39f7f4-b520-0410-a30f-b56705a9c917>
2013-02-18 15:04:00 +0000

    - Added Scene3D example

---
+++ Examples/3D/Scene3D/cache_provider.h
@@ -0,0 +1,17 @@
+
+#pragma once
+
+class ExampleSceneCacheProvider : public clan::SceneCacheProvider
+{
+public:
+	ExampleSceneCacheProvider();
+
+	std::shared_ptr<clan::ModelData> get_model_data(const std::string &name);
+
+	clan::CachedTexture get_texture(clan::GraphicContext &gc, const std::string &name, bool linear);
+
+	void update_textures(clan::GraphicContext &gc, float time_elapsed);
+
+private:
+	std::map<std::string, clan::CachedTexture> loaded_textures;
+};
---
+++ Examples/3D/Scene3D/precomp.h
@@ -0,0 +1,8 @@
+
+#pragma once
+
+#include <vector>
+#include <ClanLib/core.h>
+#include <ClanLib/display.h>
+#include <ClanLib/d3d.h>
+#include <ClanLib/scene3d.h>
