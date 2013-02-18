c77889002577c6b0227ec8c42dda3f99307e0df9
Magnus Norddahl <dpjudas@github.com>
2013-02-18 18:03:35 +0000

    - Added good old tux to the example

--- Examples/3D/Scene3D/program.cpp
+++ Examples/3D/Scene3D/program.cpp
@@ -32,7 +32,7 @@ int Program::main(const std::vector<std::string> &args)
 
 	SceneLight omni(scene);
 	omni.set_type(SceneLight::type_omni);
-	omni.set_color(Vec3f(0.0f, 0.0f, 0.5f));
+	omni.set_color(Vec3f(0.5f, 0.5f, 0.5f));
 	omni.set_position(Vec3f(100.0f, 100.0f, 100.0f));
 	omni.set_attenuation_end(200.0f);
 
@@ -42,10 +42,11 @@ int Program::main(const std::vector<std::string> &args)
 	spot.set_color(Vec3f(0.7f, 4.0f, 0.7f));
 	spot.set_falloff(45.0f);
 	spot.set_hotspot(15.0f);
-	spot.set_orientation(Quaternionf(90.0f, 45.0f, 0.0f, angle_degrees, order_YXZ));
+	spot.set_orientation(Quaternionf(90.0f, 0.0f, 0.0f, angle_degrees, order_YXZ));
 	spot.set_attenuation_end(200.0f);
 	spot.set_shadow_caster(true);
-	spot.set_rectangle_shape(true);
+	spot.set_rectangle_shape(false);
+	spot.set_aspect_ratio(1.0f);
 
 	SceneModel model(gc, scene, "plane");
 	SceneObject object(scene, model, Vec3f(0.0f, 0.0f, 0.0f));
