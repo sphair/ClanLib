edc155c898c33564e9b56a884de0a9ba1ea2c464
Magnus Norddahl <dpjudas@github.com>
2013-02-19 16:33:50 +0000

    - Updated GLSL shader

--- Resources/Scene3D/Bloom/fragment_bloom_extract.glsl
+++ Resources/Scene3D/Bloom/fragment_bloom_extract.glsl
@@ -7,7 +7,7 @@ uniform sampler2D FinalColors;
 void main()
 {
 	vec4 color = texture(FinalColors, TexCoord);
-	const float blurBegin = 0.5;
-	const float blurEnd = 1.0;
+	const float blurBegin = 1.0;
+	const float blurEnd = 2.0;
 	FragColor = vec4(clamp((color.rgb - blurBegin) / (blurEnd - blurBegin), 0, 1), 0);
 }
