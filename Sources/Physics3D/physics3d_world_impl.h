19d90f473876c17170ddf8e6d5f4d739a63a668f
mbn <mbn@cc39f7f4-b520-0410-a30f-b56705a9c917>
2013-02-20 21:26:39 +0000

    - Added Physics3DWorld initialization

--- Sources/Physics3D/physics3d_world_impl.h
+++ Sources/Physics3D/physics3d_world_impl.h
@@ -28,12 +28,25 @@
 
 #pragma once
 
+#include <memory>
+#include "Physics3D/Bullet/btBulletDynamicsCommon.h"
+#include "Physics3D/Bullet/BulletCollision/CollisionDispatch/btGhostObject.h"
+
 namespace clan
 {
 
 class Physics3DWorld_Impl
 {
 public:
+	Physics3DWorld_Impl();
+	~Physics3DWorld_Impl();
+
+	std::unique_ptr<btDefaultCollisionConfiguration> collision_configuration;
+	std::unique_ptr<btConstraintSolver> constraint_solver;
+	std::unique_ptr<btCollisionDispatcher> dispatcher;
+	std::unique_ptr<btBroadphaseInterface> broadphase;
+	std::unique_ptr<btGhostPairCallback> ghost_pair_callback;
+	std::unique_ptr<btDiscreteDynamicsWorld> dynamics_world;
 };
 
 }
