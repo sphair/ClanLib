23ac39b529ab85feaf404ee4f093d7d03666a236
Magnus Norddahl <dpjudas@github.com>
2013-02-23 14:19:11 +0000

    - Improved falling on slopes

--- Examples/3D/CharacterController/character_controller.cpp
+++ Examples/3D/CharacterController/character_controller.cpp
@@ -26,7 +26,7 @@ void CharacterController::move(const Vec3f &velocity)
 	{
 		fly_velocity -= Vec3f(0.0f, gravity, 0.0f);
 
-		if (try_move_to(position + fly_velocity))
+		if (move_vertical(position + fly_velocity))
 		{
 			flying = false;
 			fly_velocity = Vec3f();
@@ -34,14 +34,14 @@ void CharacterController::move(const Vec3f &velocity)
 	}
 	else if (velocity != Vec3f())
 	{
-		// Move position upwards to help walk on stairs and slopes:
-		try_move_to(position + Vec3f(0.0f, step_height, 0.0f));
+		// Move position upwards to help walk on stairs:
+		move_vertical(position + Vec3f(0.0f, step_height, 0.0f));
 
 		// Move position forwards:
 		move_forward(position + velocity);
 
-		// Move position downwards to help walk on stairs and slopes:
-		if (!try_move_to(position - Vec3f(0.0f, 2.0f * step_height, 0.0f)))
+		// Move position downwards to help walk on stairs:
+		if (!move_vertical(position - Vec3f(0.0f, 2.0f * step_height, 0.0f)))
 		{
 			flying = true;
 			fly_velocity = velocity * 0.5f;
@@ -55,28 +55,6 @@ void CharacterController::jump(const clan::Vec3f &jump_velocity)
 	{
 		flying = true;
 		fly_velocity = jump_velocity;
-	}
-}
-
-bool CharacterController::try_move_to(const Vec3f &target)
-{
-
-	Vec3f direction = Vec3f::normalize(target - position);
-
-	if (sweep_test.test_first_hit(shape, position, Quaternionf(), target + direction * margin, Quaternionf(), allowed_ccd_penetration))
-	{
-		Vec3f delta = sweep_test.get_hit_position(0) - position;
-		float length = std::max(delta.length() - margin, 0.0f);
-
-		position = position + direction * length;
-
-		return true;
-	}
-	else
-	{
-		position = target;
-
-		return false;
 	}
 }
 
@@ -110,6 +88,43 @@ void CharacterController::move_forward(clan::Vec3f target)
 			break;
 		}
 	}
+}
+
+bool CharacterController::move_vertical(clan::Vec3f target)
+{
+	const int max_iterations = 10;
+	float move_length = (target - position).length();
+	for (int iterations = 0; iterations < max_iterations; iterations++)
+	{
+		Vec3f direction = Vec3f::normalize(target - position);
+		if (sweep_test.test_first_hit(shape, position, Quaternionf(), target + direction * margin, Quaternionf(), allowed_ccd_penetration))
+		{
+			Vec3f hit_normal = sweep_test.get_hit_normal(0);
+			float angle = direction.y > 0.0f ? Vec3f(0.0f, 1.0f, 0.0f).dot(hit_normal) : Vec3f(0.0f, -1.0f, 0.0f).dot(hit_normal);
+			bool is_to_steep_to_stand_on = (angle < -0.5f);
+
+			if (iterations + 1 == max_iterations || is_to_steep_to_stand_on)
+			{
+				Vec3f delta = sweep_test.get_hit_position(0) - position;
+				float length = std::max(delta.length() - margin, 0.0f);
+
+				position = position + direction * length;
+				return true;
+			}
+			else
+			{
+				Vec3f reflect_dir = reflect(direction, hit_normal);
+				Vec3f perpendicular_dir = perpendicular_component(reflect_dir, hit_normal);
+				target = position + perpendicular_dir * move_length;
+			}
+		}
+		else
+		{
+			position = target;
+		}
+	}
+
+	return false;
 }
 
 Vec3f CharacterController::reflect(const Vec3f &direction, const Vec3f &normal)
