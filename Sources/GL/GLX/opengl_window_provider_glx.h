50a04844a1f9f99995fc6cc722aa52eb2cc2ab80
rombust <rombust@hotmail.co.uk>
2013-03-27 10:09:25 +0000

    Port the GL1 components of the display window provider

--- Sources/GL/GLX/opengl_window_provider_glx.h
+++ Sources/GL/GLX/opengl_window_provider_glx.h
@@ -302,6 +302,7 @@ private:
 	void create_glx_1_2(DisplayWindowSite *new_site, const DisplayWindowDescription &desc, ::Display *disp);
 	GLXContext create_context_glx_1_3_helper(GLXContext shared_context, int major_version, int minor_version, const DisplayWindowDescription &desc, ptr_glXCreateContextAttribs glXCreateContextAttribs);
 	void get_opengl_version(int &version_major, int &version_minor);
+	void update_helper(const Rect &_rect);
 
 	void on_window_resized();
 
@@ -329,6 +330,8 @@ private:
 #endif
 	bool glx_1_3;
 	OpenGLWindowDescription opengl_desc;
+	bool using_gl3;
+
 /// \}
 };
 
