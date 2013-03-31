bd1a898496735e2095ce53d6bfe1ca61c801079c
rombust <rombust@hotmail.co.uk>
2013-03-31 19:58:29 +0000

    Update the TLS ClanLib example

--- Tests/Network/TLS/test.cpp
+++ Tests/Network/TLS/test.cpp
@@ -3,22 +3,23 @@
 #include <ClanLib/network.h>
 
 #include <ctime>
+using namespace clan;
 
 void test();
 
 int main(int, char**)
 {
-	CL_SetupCore setup_core;
-	CL_SetupNetwork setup_network;
+	SetupCore setup_core;
+	SetupNetwork setup_network;
 	try
 	{
 		test();
 
 	}
-	catch (CL_Exception e)
+	catch (Exception e)
 	{
-		CL_Console::write_line(e.message);
-		CL_Console::wait_for_key();
+		Console::write_line(e.message);
+		Console::wait_for_key();
 	}
 	return 0;
 }
@@ -26,15 +27,15 @@ int main(int, char**)
 
 void test()
 {
-	CL_SocketName socket_name("encrypted.google.com", "443");
+	SocketName socket_name("encrypted.google.com", "443");
 	//CL_SocketName socket_name("www.facebook.com", "443");
 	
-	CL_TCPConnection tcp_connection(socket_name);
+	TCPConnection tcp_connection(socket_name);
 	tcp_connection.set_nodelay(true);
-	CL_TLSConnection tls_connection(tcp_connection);
+	TLSConnection tls_connection(tcp_connection);
 
-	CL_String remote_hostname = "encrypted.google.com";
-	CL_String request = cl_format(
+	std::string remote_hostname = "encrypted.google.com";
+	std::string request = string_format(
 		"GET / HTTP/1.1\r\n"
 		"Host: %1\r\n"
 		"Connection: close\r\n"
@@ -42,7 +43,7 @@ void test()
 
 	tls_connection.send(request.data(), request.length(), true);
 
-	CL_DataBuffer response(16*1024);
+	DataBuffer response(16*1024);
 	while(true)
 	{
 		bool wakeup_reason = tcp_connection.get_read_event().wait(2000);	// Wait for data
@@ -53,7 +54,7 @@ void test()
 		if (receieved == 0)
 			break;
 		//CL_Console::write(CL_String(cl_format("%1\r\n", receieved)));
-		CL_Console::write(CL_String((char *) response.get_data(), receieved));
+		Console::write(std::string((char *) response.get_data(), receieved));
 	}
 
 }
