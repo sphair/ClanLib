64b3a40d7a4baf102a87acf297439cc7e2ef3942
Kenneth Gangstoe <sphair@gmail.com>
2013-04-03 19:23:53 +0000

    - added simplest shader I could think of, which probably does not work!

--- Tests/Display/ShaderEffect/Resources/fragment_shader.glsl
+++ Tests/Display/ShaderEffect/Resources/fragment_shader.glsl
@@ -1,3 +1,6 @@
-void main() 
+out vec4 FragColor;
+
+void main()
 {
+	FragColor = vec4(1.0, 0.0, 0.0, 1.0);
 }
--- Tests/Display/ShaderEffect/Resources/vertex_shader.glsl
+++ Tests/Display/ShaderEffect/Resources/vertex_shader.glsl
@@ -1,3 +1,6 @@
-void main(void) 
+in vec4 PositionInProjection;
+
+void main()
 {
+	gl_Position = PositionInProjection;
 }
