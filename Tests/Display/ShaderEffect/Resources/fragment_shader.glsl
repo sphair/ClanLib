c28d7d3f6b15755ce6a042bc77fd85b63ae7c565
Kenneth Gangstoe <sphair@gmail.com>
2013-04-03 22:17:49 +0000

    - animating shader

--- Tests/Display/ShaderEffect/Resources/fragment_shader.glsl
+++ Tests/Display/ShaderEffect/Resources/fragment_shader.glsl
@@ -1,14 +1,17 @@
 #version 330
 
-in vec2 UV;
 out vec4 FragColor;
 
 layout(std140) uniform Uniforms
 {
-	float Blueness;
+	uniform float time;
+	uniform vec3 resolution;
 };
 
-void main()
-{
-	FragColor = vec4(UV, Blueness, 1.0);
+layout(origin_upper_left) in vec4 gl_FragCoord;
+
+void main( void ) {
+	float radius = length(gl_FragCoord.xy - resolution.xy/2.0);
+	vec4 color = vec4(48.0/radius * (1.0 + sin(time)), 24.0/radius * (1.0 + cos(time)), 6.0/radius * (1.0 + tan(time)), 1.0);
+	FragColor = color;
 }
--- Tests/Display/ShaderEffect/Resources/vertex_shader.glsl
+++ Tests/Display/ShaderEffect/Resources/vertex_shader.glsl
@@ -1,11 +1,8 @@
 #version 330
 
 in vec4 PositionInProjection;
-in vec2 AttrUV;
-out vec2 UV;
 
 void main()
 {
 	gl_Position = PositionInProjection;
-	UV = AttrUV;
 }
