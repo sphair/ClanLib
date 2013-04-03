ea0b15c222383d6eaa6f51c7a43175f7bbc150fb
Magnus Norddahl <dpjudas@github.com>
2013-04-03 21:09:07 +0000

    - OK so it didn't even compile..

--- Tests/Display/ShaderEffect/program.cpp
+++ Tests/Display/ShaderEffect/program.cpp
@@ -33,7 +33,7 @@ int Program::main(const std::vector<std::string> &args)
 		effect_description.set_vertex_shader(File::read_text("Resources/vertex_shader.glsl"));
 		effect_description.set_fragment_shader(File::read_text("Resources/fragment_shader.glsl"));
 		effect_description.set_attribute_screen_quad("PositionInProjection");
-		effect_description.set_attribute_uv_quad("AttrUV);
+		effect_description.set_attribute_uv_quad("AttrUV");
 		effect_description.set_frag_data_to_back_buffer("FragColor");
 		ShaderEffect effect(gc, effect_description);
 
