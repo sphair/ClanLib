1612ba2254b53e9a0edd14e586b4ff9a1d62d652
Kenneth Gangstoe <sphair@gmail.com>
2013-04-03 18:47:46 +0000

    - Added rest of description functions (empty, though)

--- Sources/API/Display/ShaderEffect/shader_effect_description.h
+++ Sources/API/Display/ShaderEffect/shader_effect_description.h
@@ -38,9 +38,21 @@
 #include "../Render/rasterizer_state_description.h"
 #include "../Render/blend_state_description.h"
 #include "../Render/depth_stencil_state_description.h"
+#include "../Render/storage_vector.h"
+#include "../Render/uniform_vector.h"
+#include "../Render/texture.h"
+#include "../Render/render_buffer.h"
 
 namespace clan
 {
+
+/// \brief Texture coordinate wrapping modes.
+///
+/// \xmlonly !group=Display/Display! !header=display.h! \endxmlonly
+enum UVQuadType
+{
+	uv_zero_one
+};
 
 class ShaderEffectDescription_Impl;
 
@@ -64,6 +76,7 @@ public:
 /// \{
 public:
 	void set_glsl_version(int version = 330);
+
 	void set_define(std::string name, std::string value = "1");
 
 	void set_vertex_shader(std::string code, ShaderLanguage language = shader_glsl);
@@ -73,6 +86,56 @@ public:
 	RasterizerStateDescription &rasterizer();
 	BlendStateDescription &blend();
 	DepthStencilStateDescription &depth_stencil();
+
+	void set_frag_data(std::string name, int index);
+	void set_frag_data(std::string name, RenderBuffer buffer);
+	void set_frag_data(std::string name, Texture texture);
+	void set_frag_data_screen_sized(std::string name, TextureFormat format);
+
+	void set_depth_data(TextureFormat format);
+	void set_depth_data(RenderBuffer buffer);
+	void set_depth_data(Texture texture);
+
+	void set_stencil_data(TextureFormat format);
+	void set_stencil_data(RenderBuffer buffer);
+	void set_stencil_data(Texture texture);
+
+	void set_depth_stencil_data(TextureFormat format);
+	void set_depth_stencil_data(RenderBuffer buffer);
+	void set_depth_stencil_data(Texture texture);
+
+	void set_texture(std::string name, int index);
+	void set_texture(std::string name, Texture texture);
+
+	void set_storage(std::string name, int index);
+	template<typename Type>
+	void set_storage(std::string name, std::vector<Type> values);
+	template<typename Type>
+	void set_storage(std::string name, StorageVector<Type> values);
+
+	void set_empty_storage(std::string name, int size);
+
+	void set_uniform_block(std::string name, int index);
+	template<typename Type>
+	void set_uniform_block(std::string name, Type value);
+	template<typename Type>
+	void set_uniform_block(std::string name, std::vector<Type> values);
+	template<typename Type>
+	void set_uniform_block(std::string name, UniformVector<Type> values);
+
+	template<typename Type>
+	void set_attribute(std::string name, Type *values, int count);
+	template<typename Type>
+	void set_attribute(std::string name, std::vector<Type> values);
+	template<typename Type>
+	void set_attribute(std::string name, VertexArrayVector<Type> values);
+	void set_attribute_screen_quad(std::string name);
+	void set_attribute_uv_quad(std::string name, UVQuadType type = uv_zero_one);
+
+	template<typename Type>
+	void set_elements(std::vector<Type> elements);
+	template<typename Type>
+	void set_elements(ElementArrayVector<Type> elements);
 
 /// \}
 /// \name Implementation
