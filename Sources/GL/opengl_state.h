decad9c3c7473433a38853f1fe32278a07ef39b7
rombust <rombust@hotmail.co.uk>
2013-04-13 19:43:41 +0000

    Apply experimental opengl_state.cpp/h

---
+++ Sources/GL/opengl_state.h
@@ -0,0 +1,67 @@
+/*
+**  ClanLib SDK
+**  Copyright (c) 1997-2012 The ClanLib Team
+**
+**  This software is provided 'as-is', without any express or implied
+**  warranty.  In no event will the authors be held liable for any damages
+**  arising from the use of this software.
+**
+**  Permission is granted to anyone to use this software for any purpose,
+**  including commercial applications, and to alter it and redistribute it
+**  freely, subject to the following restrictions:
+**
+**  1. The origin of this software must not be misrepresented; you must not
+**     claim that you wrote the original software. If you use this software
+**     in a product, an acknowledgment in the product documentation would be
+**     appreciated but is not required.
+**  2. Altered source versions must be plainly marked as such, and must not be
+**     misrepresented as being the original software.
+**  3. This notice may not be removed or altered from any source distribution.
+**
+**  Note: Some of the libraries ClanLib may link to may have additional
+**  requirements or restrictions.
+**
+**  File Author(s):
+**
+**    Magnus Norddahl
+**    Mark Page
+*/
+
+#pragma once
+
+#include "API/Display/TargetProviders/display_target_provider.h"
+#include "API/GL/opengl_window_description.h"
+#include "API/Display/Render/blend_state_description.h"
+#include "API/Display/TargetProviders/graphic_context_provider.h"
+#include <map>
+
+namespace clan
+{
+
+class BlendStateChangeset;
+
+class BlendStateProvider_GL : public BlendStateProvider
+{
+public:
+	BlendStateProvider_GL(const BlendStateDescription &desc);
+
+	~BlendStateProvider_GL();
+
+	BlendStateDescription desc;
+
+	static void apply(BlendStateProvider_GL *from, BlendStateProvider_GL *to);
+	
+private:
+	std::map<BlendStateProvider_GL *, std::shared_ptr<BlendStateChangeset> > changesets;
+
+	friend class BlendStateChangeset;
+};
+
+class StateChange
+{
+public:
+	virtual ~StateChange() { }
+	virtual void apply() = 0;
+};
+
+}
