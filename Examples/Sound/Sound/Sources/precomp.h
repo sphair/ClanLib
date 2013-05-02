b9dd5388eaebb95547f64c89a057a76aa7ff202d
rombust <rombust@hotmail.co.uk>
2013-05-02 20:34:46 +0000

    Fix build for Examples/Sound/Sound

--- Examples/Sound/Sound/Sources/precomp.h
+++ Examples/Sound/Sound/Sources/precomp.h
@@ -4,7 +4,6 @@
 #include <ClanLib/application.h>
 #include <ClanLib/display.h>
 #include <ClanLib/sound.h>
-#include <ClanLib/mikmod.h>
 #include <ClanLib/vorbis.h>
 #include <cmath>
 using namespace clan;
