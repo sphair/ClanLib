5899516dc2412fba90cd562f6d5f211c1ff12982
rombust <rombust@hotmail.co.uk>
2013-05-08 09:28:54 +0000

    Update the copyright date to 2013 for the entire ClanLib source.

--- Sources/Physics2D/World/physics_context_impl.cpp
+++ Sources/Physics2D/World/physics_context_impl.cpp
@@ -1,5 +1,5 @@
 /*
-**  Copyright (c) 1997-2012 The ClanLib Team
+**  Copyright (c) 1997-2013 The ClanLib Team
 **
 **  This software is provided 'as-is', without any express or implied
 **  warranty.  In no event will the authors be held liable for any damages
