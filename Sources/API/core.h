a478d7bd35a248f88729abb24c7a66e4420834ee
rombust <rombust@hotmail.co.uk>
2013-05-31 21:21:46 +0000

    Fixed missing include

--- Sources/API/core.h
+++ Sources/API/core.h
@@ -121,6 +121,7 @@
 #include "Core/Resources/resource_manager.h"
 #include "Core/Resources/xml_resource_node.h"
 #include "Core/Resources/xml_resource_document.h"
+#include "Core/Resources/xml_resource_manager.h"
 #include "Core/XML/dom_processing_instruction.h"
 #include "Core/XML/dom_entity_reference.h"
 #include "Core/XML/dom_notation.h"
