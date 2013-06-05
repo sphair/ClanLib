8a590b0864e54598741d51cc962429730fd78e9d
mbn <mbn@cc39f7f4-b520-0410-a30f-b56705a9c917>
2013-06-05 15:57:40 +0000

    - Added WorkQueue and WorkItem classes

---
+++ Sources/API/Display/Resources/work_queue.h
@@ -0,0 +1,76 @@
+/*
+**  ClanLib SDK
+**  Copyright (c) 1997-2013 The ClanLib Team
+**
+**  This software is provided 'as-is', without any express or implied
+**  warranty.  In no event will the authors be held liable for any damages
+**  arising from the use of this software.
+**
+**  Permission is granted to anyone to use this software for any purpose,
+**  including commercial applications, and to alter it and redistribute it
+**  freely, subject to the following restrictions:
+**
+**  1. The origin of this software must not be misrepresented; you must not
+**     claim that you wrote the original software. If you use this software
+**     in a product, an acknowledgment in the product documentation would be
+**     appreciated but is not required.
+**  2. Altered source versions must be plainly marked as such, and must not be
+**     misrepresented as being the original software.
+**  3. This notice may not be removed or altered from any source distribution.
+**
+**  Note: Some of the libraries ClanLib may link to may have additional
+**  requirements or restrictions.
+**
+**  File Author(s):
+**
+**    Magnus Norddahl
+*/
+
+/// \addtogroup clanDisplay_Resources clanDisplay Resources
+/// \{
+
+#pragma once
+
+#include "../api_display.h"
+#include <memory>
+
+namespace clan
+{
+
+class GraphicContext;
+
+/// \brief Interface for executing work on a worker thread
+class CL_API_DISPLAY WorkItem
+{
+public:
+	virtual ~WorkItem() { }
+
+	/// \brief Called by a worker thread to process work
+	virtual void process_work() = 0;
+
+	/// \brief Called by the WorkQueue thread to complete the work
+	virtual void work_completed(GraphicContext &gc) { }
+};
+
+class WorkQueue_Impl;
+
+/// \brief Thread pool for worker threads
+class CL_API_DISPLAY WorkQueue
+{
+public:
+	/// \brief Constructs a work queue
+	WorkQueue();
+	WorkQueue(const GraphicContext &gc);
+	~WorkQueue();
+
+	bool is_null() const { return !impl; }
+
+	void queue(WorkItem *item); // transfers ownership
+
+private:
+	std::shared_ptr<WorkQueue_Impl> impl;
+};
+
+}
+
+/// \}
