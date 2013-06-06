fdedabb592513ce1c96bbe488d512a61d301c78a
rombust <rombust@hotmail.co.uk>
2013-06-06 07:14:39 +0000

    Re-enable clanD3D for Basic2D

--- Examples/Display/Basic2D/Sources/program.cpp
+++ Examples/Display/Basic2D/Sources/program.cpp
@@ -40,7 +40,7 @@ int Program::main(const std::vector<std::string> &args)
 	clan::SetupDisplay setup_display;
 
 	// We support all display targets, in order listed here
-	//clan::SetupD3D setup_d3d;
+	clan::SetupD3D setup_d3d;
 	clan::SetupGL setup_gl;
 	clan::SetupSWRender setup_swrender;
 
