1bdc5eb57ca579c435a20a082ebbd931406412a1
rombust <rombust@hotmail.co.uk>
2013-06-06 10:28:35 +0000

    Fix the Bloom example. I'm not sure it's correct. There may be better ways... but it compiles and runs

--- Examples/Display_Shaders/Bloom/Resources/gaussian_fs.glsl
+++ Examples/Display_Shaders/Bloom/Resources/gaussian_fs.glsl
@@ -1,14 +1,21 @@
-varying vec2 TexCoord;
-uniform vec2 SampleOffsets[15];
-uniform float SampleWeights[15];
+#version 150
+in vec2 TexCoord;
+out vec4 cl_FragColor;
+
 uniform sampler2D SourceTexture;
+
+layout (std140) uniform ProgramUniforms
+{
+	mat4 cl_ModelViewProjectionMatrix;
+	vec4 Sample[15];	// x,y = Offset. z = Weight
+};
 
 vec4 GaussianBlur()
 {
 	vec4 c = vec4(0.0);
 	for (int i = 0 ; i < 15; i++)
 	{
-		c += texture2D(SourceTexture, (TexCoord + SampleOffsets[i])) * SampleWeights[i];
+		c += texture2D(SourceTexture, (TexCoord + Sample[i].xy)) * Sample[i].z;
 	}
 
 	return c;
@@ -16,5 +23,5 @@ vec4 GaussianBlur()
 
 void main()
 {
-	gl_FragColor = GaussianBlur();
+	cl_FragColor = GaussianBlur();
 }
--- Examples/Display_Shaders/Bloom/Sources/app.h
+++ Examples/Display_Shaders/Bloom/Sources/app.h
@@ -35,13 +35,35 @@ public:
 
 private:
 	float compute_gaussian(float n, float theta);
-	void render_gaussian_blur(clan::Canvas &canvas, clan::Texture2D &source_texture, clan::ProgramObject &program_object, float dx, float dy);
+	void render_gaussian_blur(clan::Canvas &canvas, float blur_amount, clan::Texture2D &source_texture, clan::ProgramObject &program_object, float dx, float dy);
 	void on_input_up(const clan::InputEvent &key);
-	void draw_texture(clan::Canvas &canvas, const clan::Rectf &rect, const clan::Colorf &color, const clan::Rectf &texture_unit1_coords);
+	void draw_texture(clan::GraphicContext &gc, const clan::Rectf &rect, const clan::Rectf &texture_unit1_coords);
 	void render_extract_highlights(clan::Canvas &canvas, clan::Texture2D &source_texture, clan::ProgramObject &program_object);
 	void render_bloom_combine(clan::Canvas &canvas, clan::Texture2D &tex_base, clan::Texture2D &tex_bloom, clan::ProgramObject &program_object);
 
 	bool quit;
+
+	static const int sampleCount = 15;
+
+	struct Sample
+	{
+		float offset_x;
+		float offset_y;
+		float weight;
+		float padding;
+	};
+
+	struct ProgramUniforms
+	{
+		clan::Mat4f cl_ModelViewProjectionMatrix;
+		Sample sample[sampleCount];		// Each array element must be aligned to a vec4 (4 floats) in GLSL
+	};
+
+	ProgramUniforms uniforms;
+	clan::VertexArrayVector<clan::Vec2f> gpu_positions;
+	clan::VertexArrayVector<clan::Vec2f> gpu_tex1_coords;
+	clan::UniformVector<ProgramUniforms> gpu_uniforms;
+	clan::PrimitivesArray gpu_primitives_array;
 
 	std::string select_text;
 	float highlight_threshold;
