ef6fa7afe22c787d17597e8212ea139d874a7938
rombust <rombust@hotmail.co.uk>
2013-06-06 07:14:39 +0000

    Re-enable clanD3D for Basic2D

--- Examples/Display/Basic2D/Sources/precomp.h
+++ Examples/Display/Basic2D/Sources/precomp.h
@@ -3,7 +3,7 @@
 #include <ClanLib/core.h>
 #include <ClanLib/application.h>
 #include <ClanLib/display.h>
-//#include <ClanLib/d3d.h>
+#include <ClanLib/d3d.h>
 #include <ClanLib/swrender.h>
 #include <ClanLib/gl.h>
 #include <cmath>
