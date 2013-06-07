fc391edbf2c0c36445c08c6848829e36aca7241b
Magnus Norddahl <dpjudas@github.com>
2013-06-07 14:23:34 +0000

    - Ported some of the HLSL shaders to GLSL

--- Resources/Scene3D/GBuffer/fragment_gbuffer.glsl
+++ Resources/Scene3D/GBuffer/fragment_gbuffer.glsl
@@ -1,20 +1,19 @@
 
 #define DiffuseUV UVMap0
 #define BumpMapUV UVMap1
-#define LightMapUV UVMap2
+#define SelfIlluminationUV UVMap2
 #define SpecularUV UVMap3
 
-layout(std140) uniform ModelUniformBlock
+layout(std140) uniform ModelMaterialUniforms
 {
-	mat4 UVTextureMatrix0;
-	mat4 UVTextureMatrix1;
-	mat4 UVTextureMatrix2;
-	mat4 UVTextureMatrix3;
 	vec4 MaterialAmbient;
 	vec4 MaterialDiffuse;
 	vec4 MaterialSpecular;
 	float MaterialGlossiness;
 	float MaterialSpecularLevel;
+	uint ModelIndex;
+	uint VectorsPerInstance;
+	uint MaterialOffset;
 };
 
 in vec3 NormalInEye;
@@ -22,118 +21,103 @@ in vec3 BitangentInEye;
 in vec3 TangentInEye;
 in vec4 PositionInWorld;
 in vec4 PositionInEye;
-
+#if defined(DIFFUSE_UV)
 in vec2 UVMap0;
+#endif
+#if defined(BUMPMAP_UV)
 in vec2 UVMap1;
+#endif
+#if defined(SI_UV)
 in vec2 UVMap2;
+#endif
+#if defined(SPECULAR_UV)
 in vec2 UVMap3;
+#endif
+in float ArrayTextureIndex;
+in vec4 SelfIllumination;
+#if defined(USE_COLORS)
+in vec4 VertexColor;
+#endif
+in vec4 LightProbeColor;
 
-uniform sampler2D DiffuseTexture;
-uniform sampler2D BumpMapTexture;
-uniform sampler2D LightMapTexture;
-uniform sampler2D SpecularTexture;
-
-out vec3 FragDiffuseColor;
+out vec4 FragDiffuseColor;
 out vec4 FragSpecularColor;
 out vec2 FragSpecularLevel;
+out vec4 FragSelfIllumination;
 out vec4 FragNormal;
 
-vec3 NormalInEyeNormalized;
+#if defined(DIFFUSE_ARRAY)
+uniform sampler2DArray DiffuseTexture;
+#else
+uniform sampler2D DiffuseTexture;
+#endif
+uniform sampler2D BumpMapTexture;
+uniform sampler2D SelfIlluminationTexture;
+uniform sampler2D SpecularTexture;
 
-void ApplyNormalMap();
-vec3 AmbientColor();
+vec3 ApplyNormalMap(bool frontFacing);
 vec3 DiffuseColor();
 vec3 SpecularColor();
+vec3 SelfIlluminationColor(vec4 diffuseColor);
 
 void main()
 {
-	ApplyNormalMap();
-	FragDiffuseColor = DiffuseColor();
+	vec3 normalInEyeNormalized = ApplyNormalMap(gl_FrontFacing);
+	FragDiffuseColor = vec4(DiffuseColor(), 1);
 	FragSpecularColor = vec4(SpecularColor(), 1);
 	FragSpecularLevel = vec2(MaterialGlossiness, MaterialSpecularLevel);
-	FragNormal = vec4(NormalInEyeNormalized, PositionInEye.z);
+	FragSelfIllumination = vec4(FragDiffuseColor.rgb * LightProbeColor.rgb + SelfIlluminationColor(output.FragDiffuseColor), 1);
+	FragNormal = vec4(normalInEyeNormalized, PositionInEye.z);
 }
 
 #if defined(BUMPMAP_UV)
 
-void ApplyNormalMap()
+vec3 ApplyNormalMap(bool frontFacing)
 {
-	vec3 frontNormal = gl_FrontFacing ? NormalInEye : -NormalInEye;
-	vec3 frontTangent = gl_FrontFacing ? TangentInEye : -TangentInEye;
-	vec3 frontBitangent = gl_FrontFacing ? BitangentInEye : -BitangentInEye;
+	// Get normal vector in tangent space from normal map:
+	vec3 normalInTangent = texture(BumpMapTexture, BumpMapUV).rgb * 2 - 1;
 
-#if defined(USE_DF_FUNCTIONS)
-	vec3 q0 = dFdx(PositionInEye.xyz);
-	vec3 q1 = dFdy(PositionInEye.xyz);
-	vec2 st0 = dFdx(BumpMapUV.st);
-	vec2 st1 = dFdy(BumpMapUV.st);
-	vec3 S = normalize( q0 * st1.t - q1 * st0.t);
-	vec3 T = normalize(-q0 * st1.s + q1 * st0.s);
-	vec3 normal = texture2D(BumpMapTexture, BumpMapUV).rgb * 2 - 1;
-	NormalInEyeNormalized = normalize(normal.x * S + normal.y * T + normal.z * frontNormal);
+	// Re-normalize normal vector in tangent space (gives smoother result and allows us to also only use a RG normal texture)
+	normalInTangent.z = sqrt(1 - dot(normalInTangent.xy, normalInTangent.xy));
 
-#else
+	// Find normal, tangent and bitang vectors for this fragment (taking face direction into account):
+	vec3 frontNormal = normalize(frontFacing ? NormalInEye : -NormalInEye);
+	vec3 frontTangent = normalize(frontFacing ? TangentInEye : -TangentInEye);
+	vec3 frontBitangent = normalize(frontFacing ? BitangentInEye : -BitangentInEye);
 
-#if defined(PARALLAX_EFFECT)
-	float holeDepth = 0.2;
-	mat3 ToTangentSpace = mat3(frontBitangent, frontTangent, frontNormal); // left hand system
-	//mat3 ToTangentSpace = mat3(frontTangent, frontBitangent, frontNormal); // right hand system
+	// Calculate normal adjusted by the normal map:
+	vec3 bumpedNormal = normalInTangent.x * frontTangent - normalInTangent.y * frontBitangent + normalInTangent.z * frontNormal;
 
-	//float depth = (texture2D(BumpMapTexture, BumpMapUV).a) * holeDepth;
-	//vec2 offset = depth * EyeToTangentSpace * BumpMapUV;
-
-	vec2 offset = holeDepth * ToTangentSpace * BumpMapUV;
-	float depth_at_1 = texture2D(BumpMapTexture, BumpMapUV + offset).w;
-	if (depth_at_1 < 0.96f)
-		offset *= (depth_at_1 + (texture2D(BumpMapTexture, BumpMapUV).w)) * 0.5;
-
-	BumpMapUV += offset;
-	DiffuseUV += offset;
-#endif
-/*
-	vec3 normal = normalize(2.0 * texture2D(BumpMapTexture, BumpMapUV).rgb - 1.0);
-	normal = vec3(dot(normal, frontTangent), dot(normal, frontBitangent), dot(normal, frontNormal));
-	NormalInEyeNormalized = normalize(normal);
-*/
-
-	float bumpAmount = 0.30;
-	vec3 normal = texture2D(BumpMapTexture, BumpMapUV).rgb * 2 - 1;
-	//vec3 normal = texture2D(BumpMapTexture, BumpMapUV).rgb;
-	//normal.xy = normal.xy * 2.0 - 1.0; // transform to [-1,1] range
-	//normal.z = sqrt(1.0 - dot(normal.xy, normal.xy)); // compute z component
-	normal = normal.x * frontTangent - normal.y * frontBitangent + normal.z * frontNormal;
-	NormalInEyeNormalized = normalize(mix(frontNormal, normal, bumpAmount));
-#endif
+	// Mix between the original normal vector and the bumped normal vector according to the material properties
+	float bumpAmount = 0.30; // Hardcoded to the 3ds max default for now
+	return normalize(mix(frontNormal, bumpedNormal, bumpAmount));
 }
 
 #else
 
-void ApplyNormalMap()
+vec3 ApplyNormalMap(bool frontFacing)
 {
-	vec3 frontNormal = gl_FrontFacing ? NormalInEye : -NormalInEye;
-	NormalInEyeNormalized = normalize(frontNormal);
+	vec3 frontNormal = frontFacing ? NormalInEye : -NormalInEye;
+	return normalize(frontNormal);
 }
 
 #endif
 
 #if defined(DIFFUSE_UV)
 
-vec3 AmbientColor()
-{
-	return texture2D(DiffuseTexture, DiffuseUV).rgb;
-}
-
 vec3 DiffuseColor()
 {
-	return texture2D(DiffuseTexture, DiffuseUV).rgb;
+#if defined(DIFFUSE_ARRAY)
+	vec4 color = texture(DiffuseTexture, DiffuseUV, ArrayTextureIndex);
+#else
+	vec4 color = texture(DiffuseTexture, DiffuseUV);
+#endif
+	if (color.a < 0.5)
+		discard;
+	return color.rgb;
 }
 
 #else
-
-vec3 AmbientColor()
-{
-	return MaterialAmbient.xyz;
-}
 
 vec3 DiffuseColor()
 {
@@ -146,7 +130,7 @@ vec3 DiffuseColor()
 
 vec3 SpecularColor()
 {
-	return LightSpecular * texture2D(SpecularTexture, SpecularUV).rgb;
+	return texture(SpecularTexture, SpecularUV).rgb;
 }
 
 #else
@@ -154,6 +138,31 @@ vec3 SpecularColor()
 vec3 SpecularColor()
 {
 	return MaterialSpecular.xyz;
+}
+
+#endif
+
+#if defined(SI_UV)
+
+vec3 SelfIlluminationColor(vec4 diffuseColor)
+{
+#if defined(USE_COLORS)
+	vec3 siColor = SelfIllumination.rgb + VertexColor.rgb * diffuseColor.rgb;
+	return mix(siColor, texture(SelfIlluminationTexture, SelfIlluminationUV).rgb, SelfIllumination.w);
+#else
+	return mix(SelfIllumination.rgb, texture(SelfIlluminationTexture, SelfIlluminationUV).rgb, SelfIllumination.w);
+#endif
+}
+
+#else
+
+vec3 SelfIlluminationColor(vec4 diffuseColor)
+{
+#if defined(USE_COLORS)
+	return SelfIllumination.rgb + VertexColor.rgb * diffuseColor.rgb;
+#else
+	return SelfIllumination.rgb;
+#endif
 }
 
 #endif
