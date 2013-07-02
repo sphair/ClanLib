cf851c0c1b419c66cce366536cfc97d669fb03b7
rombust <rombust@hotmail.co.uk>
2013-07-02 07:33:30 +0000

    Add is_equal function to the vector classes

--- Sources/API/Core/Math/vec4.h
+++ Sources/API/Core/Math/vec4.h
@@ -150,6 +150,17 @@ public:
 	/// \return The rounded vector
 	static Vec4<Type> round(const Vec4<Type>& vector);
 
+	/// \brief Returns true if equal within the bounds of an epsilon
+	///
+	/// \param first = Value A
+	/// \param second = Value B
+	/// \param epsilon = The epsilon (eg FLT_EPSILON/2, DBL_EPSILON/2)
+	static bool is_equal(const Vec4<Type> &first, const Vec4<Type> &second, Type epsilon)
+	{
+		Type diff_x = second.x - first.x; Type diff_y = second.y - first.y; Type diff_z = second.z - first.z; Type diff_w = second.w - first.w;
+		return (diff_x >= -epsilon && diff_x <= epsilon && diff_y >= -epsilon && diff_y <= epsilon && diff_z >= -epsilon && diff_z <= epsilon && diff_w >= -epsilon && diff_w <= epsilon );
+	}
+
 /// \name Attributes
 /// \{
 
@@ -241,6 +252,12 @@ public:
 	/// Uses Asymmetric Arithmetic Rounding
 	/// \return reference to this object
 	Vec4<Type> &round();
+
+	/// \brief Returns true if equal within the bounds of an epsilon
+	///
+	/// \param other = Other value
+	/// \param epsilon = The epsilon (eg FLT_EPSILON/2, DBL_EPSILON/2)
+	bool is_equal(const Vec4<Type> &other, Type epsilon) const { return Vec4<Type>::is_equal(*this, other, epsilon);	}
 
 /// \}
 /// \name Operators
