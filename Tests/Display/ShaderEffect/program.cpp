636d6889f68ce2f5e20b98e1c0b0ab321e49723f
rombust <rombust@example.com>
2013-08-19 14:56:38 +0100

    Update the ShaderEffect test so it compiles

--- Tests/Display/ShaderEffect/program.cpp
+++ Tests/Display/ShaderEffect/program.cpp
@@ -20,54 +20,43 @@ int Program::main(const std::vector<std::string> &args)
 	SetupDisplay setup_display;
 	SetupGL setup_gl;
 
-	ConsoleWindow console_window("Debug");
+	DisplayWindow window("Hello ShaderEffect", 800, 600, false, true);
+	Slot slot = window.sig_window_close().connect(&Program::exit_func);
 
-	try
+	GraphicContext gc = window.get_gc();
+	InputDevice mouse = window.get_ic().get_mouse();
+	Texture2D texture(gc, "../../../Examples/GUI/CommonCode/Resources/tux.png");
+
+	struct Uniforms
 	{
-		DisplayWindow window("Hello ShaderEffect", 800, 600, false, true);
-		Slot slot = window.sig_window_close().connect(&Program::exit_func);
+		Vec3f resolution;
+		float time;
+		Vec4f mouse;
+	};
 
-		GraphicContext gc = window.get_gc();
-		InputDevice mouse = window.get_ic().get_mouse();
-		Texture2D texture(gc, "../2D/tux.png");
+	Uniforms uniforms;
+	uniforms.time = 0.0f;
+	uniforms.resolution = Vec3f(800, 600, 0);
+	UniformVector<Uniforms> uniformVector(gc, &uniforms, 1);
 
-		struct Uniforms
-		{
-			Vec3f resolution;
-			float time;
-			Vec4f mouse;
-		};
+	ShaderEffectDescription effect_description;
+	effect_description.set_vertex_shader(File::read_text("Resources/vertex_shader.glsl"));
+	effect_description.set_fragment_shader(File::read_text("Resources/fragment_shader.glsl"));
+	effect_description.set_attribute_screen_quad("PositionInProjection");
+	effect_description.set_frag_data_to_back_buffer("FragColor");
+	effect_description.set_uniform_block("Uniforms", uniformVector);
+	effect_description.set_texture("Tux", texture);
+	ShaderEffect effect(gc, effect_description);
 
-		Uniforms uniforms;
-		uniforms.time = 0.0f;
-		uniforms.resolution = Vec3f(800, 600, 0);
-		UniformVector<Uniforms> uniformVector(gc, &uniforms, 1);
-
-		ShaderEffectDescription effect_description;
-		effect_description.set_vertex_shader(File::read_text("Resources/vertex_shader.glsl"));
-		effect_description.set_fragment_shader(File::read_text("Resources/fragment_shader.glsl"));
-		effect_description.set_attribute_screen_quad("PositionInProjection");
-		effect_description.set_frag_data_to_back_buffer("FragColor");
-		effect_description.set_uniform_block("Uniforms", uniformVector);
-		effect_description.set_texture("Tux", texture);
-		ShaderEffect effect(gc, effect_description);
-
-		while (!exit)
-		{
-			uniforms.time = System::get_time() / 1000.0f;
-			uniforms.mouse = Vec4f(mouse.get_x() / 800.0f, mouse.get_y() / 600.0f, 0, 0);
-			uniformVector.upload_data(gc, &uniforms, 1);
-
-			effect.draw(gc);
-			window.flip(0);
-			KeepAlive::process();
-		}
-	}
-	catch (Exception &e)
+	while (!exit)
 	{
-		Console::write_line("Unhandled exception: %1", e.get_message_and_stack_trace());
-		Console::wait_for_key();
-		return -1;
+		uniforms.time = System::get_time() / 1000.0f;
+		uniforms.mouse = Vec4f(mouse.get_x() / 800.0f, mouse.get_y() / 600.0f, 0, 0);
+		uniformVector.upload_data(gc, &uniforms, 1);
+
+		effect.draw(gc);
+		window.flip(0);
+		KeepAlive::process();
 	}
 
 	return 0;
