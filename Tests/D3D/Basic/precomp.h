32d08696441ac739b3a42187bb16e7201d103250
rombust <rombust@example.com>
2013-08-19 16:10:56 +0100

    Drop D3D test, GUI Button2 and 3 test and almost fix the Button1 test

--- Tests/D3D/Basic/precomp.h
+++
@@ -1,9 +0,0 @@
-
-#pragma once
-
-#include <vector>
-#include <ClanLib/core.h>
-#include <ClanLib/display.h>
-#include <ClanLib/d3d.h>
-#include <ClanLib/compute.h>
-#include <ClanLib/gl.h>
--- Tests/GUI/Button1/test.cpp
+++ Tests/GUI/Button1/test.cpp
@@ -16,20 +16,15 @@ public:
 		{
 			GUIManager gui;
 
-			DisplayCache resources("../../../Resources/GUIThemeLuna/resources.xml");
-
 			GUIWindowManagerSystem wm;
 			gui.set_window_manager(wm);
-
-			GUIThemeDefault theme;
-			theme.set_resources(resources);
-			gui.set_theme(theme); 
-			gui.set_css_document("theme.css");
-
+			gui.add_resources(clan::XMLResourceDocument("../../../Resources/GUIThemeAero/resources.xml"));
+			gui.add_theme("../../../Resources/GUIThemeAero");
 			DisplayWindowDescription win_desc;
 			win_desc.set_allow_resize(true);
 			win_desc.set_title("Button WM System Test Application");
 			win_desc.set_position(Rect(200, 200, 540, 440), false);
+
 			Window window(&gui, win_desc);
 			window.func_close().set(this, &App::on_close, &window);
 
@@ -43,27 +38,6 @@ public:
 			button_disabled.func_clicked().set(this, &App::on_button_clicked, &button_disabled);
 			button_disabled.set_text("Button Disabled");
 			button_disabled.set_enabled(false);
-
-			PushButton button_ugly_flat(&window);
-			button_ugly_flat.set_geometry(Rect(170, 110, 330, 150));
-			button_ugly_flat.func_clicked().set(this, &App::on_button_clicked, &button_ugly_flat);
-			button_ugly_flat.set_flat(true);
-			button_ugly_flat.set_class_name("ugly");
-			button_ugly_flat.set_text("Button Ugly Flat");
-
-			PushButton button_ugly(&window);
-			button_ugly.set_geometry(Rect(170, 10, 330, 50));
-			button_ugly.func_clicked().set(this, &App::on_button_clicked, &button_ugly);
-			button_ugly.set_flat(false);
-			button_ugly.set_class_name("ugly");
-			button_ugly.set_text("Button Ugly");
-
-			PushButton button_ugly_disabled(&window);
-			button_ugly_disabled.set_geometry(Rect(170, 60, 330, 100));
-			button_ugly_disabled.func_clicked().set(this, &App::on_button_clicked, &button_ugly_disabled);
-			button_ugly_disabled.set_class_name("ugly");
-			button_ugly_disabled.set_text("Button Ugly Disabled");
-			button_ugly_disabled.set_enabled(false);
 
 			label = new Label(&window);
 			label->set_geometry(Rect(10, 160, 330, 180));
