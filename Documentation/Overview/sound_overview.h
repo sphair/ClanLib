ea708ff12e66f2c401c6cc3c3e19768b1c74d2be
Mark Page <rombust@server.fake>
2013-09-20 10:23:20 +0100

    Update sound_overview.h. Still requires porting to 3.0 by somebody who understands it

--- Documentation/Overview/sound_overview.h
+++ Documentation/Overview/sound_overview.h
@@ -1,86 +1,75 @@
 
 /*! \page SoundOverview Sound Overview
 
-MainDocs:Sound
-
-From ClanLib Game SDK
- 
+*** THIS PAGE REQUIRES UPDATING FOR CLANLIB 3.0 ***
+<br/>
 
 ClanSound provides you with easy-to-use and powerful sound functionality. 
 
 
+<h2>Sound overview</h2>
 
-Contents
-•1 Sound overview
-•2 Playing wav samples
-•3 Playing music streams
-•4 Resources
-•5 Sessions
-•6 Filters
-•7 Sound providers
- 
-
-Sound overview
-
-To play sound in ClanLib, you have to initialize CL_SetupSound and also create a CL_SoundOutput. CL_SoundOutput is the interface to a sound output device, which is used to control the main mixer volume and other global settings. It is used as a singleton. 
+To play sound in ClanLib, you have to initialize \ref clan::SetupSound and also create a \ref clan::SoundOutput. \ref clan::SoundOutput is the interface to a sound output device, which is used to control the main mixer volume and other global settings. It is used as a singleton. 
 
 
- 
+\code 
 	#include <ClanLib/sound.h> 
  
 	...
  
-	CL_SetupSound setup_sound;
-	CL_SoundOutput output(44100);
- 
+	clan::SetupSound setup_sound;
+	clan::SoundOutput output(44100);
+\endcode
 	...
 
-Playing wav samples
+<hr>Playing wav samples</h2>
 
-A CL_SoundBuffer contains a sample or music stream. To load and play a WAV file, you do the following: 
+A \ref clan::SoundBuffer contains a sample or music stream. To load and play a WAV file, you do the following: 
 
 
- 
+\code 
 	// Load a sample from a wave file:
-	CL_SoundBuffer sample("beep.wav");
+	clan::SoundBuffer sample("beep.wav");
  
 	// Play sample
 	sample.play();
+\endcode
 
 You can set the volume and panning of soundbuffers. This will change the default attributes of the soundbuffer, and will be effective on all subsequent calls to play(). 
 
 
- 
+\code 
 	// Turn down volume on this soundbuffer.
 	sample.set_volume(0.1f);
  
 	// Play only in left speaker.
 	sample.set_pan(-1.0f);
+\endcode
 
 Note that setting these attributes don't affect already playing soundbuffers, only when you call play() again afterwards. To have individual control over each playing sound, you need to use a sound session - read about that further down. 
 
-Playing music streams
+<h2>Playing music streams</h2>
 
-There isn't much difference between playing wav samples and music stream. ClanSound itself doesn't provide more than wav samples playback, so you'll need one or more additional music modules. ClanLib currently has two, MikMod and Vorbis. They are initialized using CL_SetupMikMod and CL_SetupVorbis. 
+There isn't much difference between playing wav samples and music stream. ClanSound itself doesn't provide more than wav samples playback, so you'll need one or more additional music modules. ClanLib currently has two, MikMod and Vorbis. They are initialized using \ref clan::SetupMikMod and \ref clan::SetupVorbis. 
 
-
- 
-	#include <ClanLib/sound.h> 
+\code
+ 	#include <ClanLib/sound.h> 
 	#include <ClanLib/vorbis.h> 
 	#include <ClanLib/mikmod.h> 
  
-	CL_SetupSound setup_sound;
-	CL_SetupMikMod setup_mikmod;
-	CL_SetupVorbis setup_vorbis;
+	clan::SetupSound setup_sound;
+	clan::SetupMikMod setup_mikmod;
+	clan::SetupVorbis setup_vorbis;
  
-	CL_SoundOutput output(44100);
+	clan::SoundOutput output(44100);
+\endcode
 
 Then to play music, you use the same approach as with wav samples: 
 
 
- 
+\code 
 	// Load ogg file into a soundbuffer
-	CL_SoundBuffer vorbis("cheer1.ogg");
+	clan::SoundBuffer vorbis("cheer1.ogg");
  
 	// Play vorbis stream
 	vorbis.play();
@@ -88,92 +77,97 @@ Then to play music, you use the same approach as with wav samples:
 	...
  
 	// Load an XM mod into a soundbuffer
-	CL_SoundBuffer mod("clanbeat.xm");
+	clan::SoundBuffer mod("clanbeat.xm");
  
 	// Play mod
 	mod.play();
+\endcode
 
-Resources
+<h2>Resources</h2>
 
 TODO: Write about loading samples from resources. 
 
-Sessions
+<h2>Sessions</h2>
 
-If you want to control the playback of sound, or want to know if the sound is still playing, you use CL_SoundBuffer_Session. This can be created in two ways, either by calling CL_SoundBuffer::play() or CL_SoundBuffer::prepare(). The difference between prepare() and play() is that prepare will just load the sound, but not play it. You can then call play() from your resulting CL_SoundBuffer_Session. 
+If you want to control the playback of sound, or want to know if the sound is still playing, you use \ref clan::SoundBuffer_Session. This can be created in two ways, either by calling \ref clan::SoundBuffer::play() or \ref clan::SoundBuffer::prepare(). The difference between prepare() and play() is that prepare will just load the sound, but not play it. You can then call play() from your resulting \ref clan::SoundBuffer_Session. 
 
 
- 
+ \code
 	// Load ogg file into a soundbuffer
-	CL_SoundBuffer vorbis("cheer1.ogg");
+	clan::SoundBuffer vorbis("cheer1.ogg");
  
 	// Create a session from soundbuffer.
-	CL_SoundBuffer_Session playback = vorbis.play();
+	clan::SoundBuffer_Session playback = vorbis.play();
  
 	..
  
 	// Load ogg file into a soundbuffer
-	CL_SoundBuffer vorbis("cheer1.ogg");
+	clan::SoundBuffer vorbis("cheer1.ogg");
  
 	// Create a session from soundbuffer.
-	CL_SoundBuffer_Session playback = vorbis.prepare();
+	clan::SoundBuffer_Session playback = vorbis.prepare();
  
 	playback.play();
+\endcode
 
 When you have a session object, you can modify the attributes of the playing sound in many ways. These take effect immidiately, you can change it while it is playing. You can set the volume, panning, position, and frequency (speed). You can make it loop, and adding filters. You can query if it is still playing, and you can ofcourse start and stop it as you want. 
 
-
+\code
  
 	// Load ogg file into a soundbuffer
-	CL_SoundBuffer vorbis("cheer1.ogg");
+	clan::SoundBuffer vorbis("cheer1.ogg");
  
 	// Create a session from soundbuffer.
-	CL_SoundBuffer_Session playback = vorbis.prepare();
+	clan::SoundBuffer_Session playback = vorbis.prepare();
  
 	playback.set_volume(0.5f);
 	playback.set_looping(true);
 	playback.play();
+\endcode
 
-Filters
+<h2>Filters</h2>
 
-If you want to modify sounds in special, dynamic ways, you can apply a CL_SoundFilter to it. 
+If you want to modify sounds in special, dynamic ways, you can apply a \ref clan::SoundFilter to it. 
 
-ClanLib has three builtin filters: CL_EchoFilter, CL_FadeFilter and CL_InverseEchoFilter. The usefulness of an CL_InverseEchoFilter can be discussed :) 
+ClanLib has three builtin filters: \ref clan::EchoFilter, \ref clan::FadeFilter and \ref clan::InverseEchoFilter. The usefulness of an \ref clan::InverseEchoFilter can be discussed :) 
 
 Example of the fade filter: 
 
-
+\code
  
 	// Load ogg file into a soundbuffer
-	CL_SoundBuffer vorbis("cheer1.ogg");
+	clan::SoundBuffer vorbis("cheer1.ogg");
  
 	// Create a session from soundbuffer.
-	CL_SoundBuffer_Session playback = vorbis.prepare();
+	clan::SoundBuffer_Session playback = vorbis.prepare();
  
-	CL_FadeFilter fade(0.0f);
+	clan::FadeFilter fade(0.0f);
  
 	playback.add_filter(&fade);
 	playback.play();
  
 	fade.fade_to_volume(1.0f, 10*1000); // fade in, duration 10 seconds.
+\endcode
 
 Example of the echo filter: 
 
 
- 
+\code 
 	// Load ogg file into a soundbuffer
-	CL_SoundBuffer vorbis("cheer1.ogg");
+	clan::SoundBuffer vorbis("cheer1.ogg");
  
 	// Create a session from soundbuffer.
-	CL_SoundBuffer_Session playback = vorbis.prepare();
+	clan::SoundBuffer_Session playback = vorbis.prepare();
  
-	playback.add_filter(new CL_EchoFilter(8*1024), true);
+	playback.add_filter(new clan::EchoFilter(8*1024), true);
 	playback.play();
+\endcode
 
-You can ofcourse also code your own filters. Check the SoundFilters example how do to that - it creates a CL_DistortFilter. 
+You can ofcourse also code your own filters. Check the SoundFilters example how do to that - it creates a \ref clan::DistortFilter. 
 
-It is also possible to add filters on mixer level too. Ie fade all sound out alltogether. Use CL_SoundOutput::add_filter() just like with CL_SoundBuffer_Session. 
+It is also possible to add filters on mixer level too. Ie fade all sound out alltogether. Use \ref clan::SoundOutput::add_filter() just like with \ref clan::SoundBuffer_Session. 
 
-Sound providers
+<h2>Sound providers</h2>
 
 It is also possible to create sound on-the-fly, or adding extra sound providers. See the StreamSoundProvider example for code how to do this. 
 
