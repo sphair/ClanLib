b96f717ce4173989c0267d4118bf82d5fe294eec
Mark Page <rombust@server.fake>
2013-09-20 10:37:46 +0100

    Update texture_resource.h

--- Documentation/Overview/texture_resource.h
+++ Documentation/Overview/texture_resource.h
@@ -1,16 +1,9 @@
 
 /*! \page TextureResource Texture Resource
 
-MainDocs:Texture Resources
-
-From ClanLib Game SDK
- 
-
 Resource options
 
-
- 
-   <texture name="mytexture" file="filename.png" />
+\<texture name="mytexture" file="filename.png" /\>
 
 The name and file attributes are required to define a texture in resources. 
 
