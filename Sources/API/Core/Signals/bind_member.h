dc36170f340cbcb8a013208dcdb83c6614b5f5bf
Mark Page <rombust@server.fake>
2014-07-02 07:41:10 +0100

    Update bind_member.h using a more flexible template

--- Sources/API/Core/Signals/bind_member.h
+++ Sources/API/Core/Signals/bind_member.h
@@ -24,6 +24,7 @@
 **  File Author(s):
 **
 **    Magnus Norddahl
+**    Marcel Hellwig
 */
 
 #pragma once
@@ -33,71 +34,7 @@ namespace clan
 {
 /// \addtogroup clanCore_Signals clanCore Signals
 /// \{
-
-	template<typename R, typename T>
-	std::function<R()> bind_member(T *instance, R(T::*func)()) { return [=]() -> R { return (instance->*func)(); }; }
-
-	template<typename R, typename T>
-	std::function<void()> bind_member(T *instance, void(T::*func)()) { return [=]() { (instance->*func)(); }; }
-
-	template<typename R, typename T, typename Arg1>
-	std::function<R(Arg1)> bind_member(T *instance, R(T::*func)(Arg1)) { return [=](Arg1 arg1) -> R { return (instance->*func)(arg1); }; }
-
-	template<typename R, typename T, typename Arg1>
-	std::function<void(Arg1)> bind_member(T *instance, void(T::*func)(Arg1)) { return [=](Arg1 arg1) { (instance->*func)(arg1); }; }
-
-	template<typename R, typename T, typename Arg1, typename Arg2>
-	std::function<R(Arg1, Arg2)> bind_member(T *instance, R(T::*func)(Arg1, Arg2)) { return [=](Arg1 arg1, Arg2 arg2) -> R { return (instance->*func)(arg1, arg2); }; }
-
-	template<typename R, typename T, typename Arg1, typename Arg2>
-	std::function<void(Arg1, Arg2)> bind_member(T *instance, void(T::*func)(Arg1, Arg2)) { return [=](Arg1 arg1, Arg2 arg2) { (instance->*func)(arg1, arg2); }; }
-
-	template<typename R, typename T, typename Arg1, typename Arg2, typename Arg3>
-	std::function<R(Arg1, Arg2, Arg3)> bind_member(T *instance, R(T::*func)(Arg1, Arg2, Arg3)) { return [=](Arg1 arg1, Arg2 arg2, Arg3 arg3) -> R { return (instance->*func)(arg1, arg2, arg3); }; }
-
-	template<typename R, typename T, typename Arg1, typename Arg2, typename Arg3>
-	std::function<void(Arg1, Arg2, Arg3)> bind_member(T *instance, void(T::*func)(Arg1, Arg2, Arg3)) { return [=](Arg1 arg1, Arg2 arg2, Arg3 arg3) { (instance->*func)(arg1, arg2, arg3); }; }
-
-	template<typename R, typename T, typename Arg1, typename Arg2, typename Arg3, typename Arg4>
-	std::function<R(Arg1, Arg2, Arg3, Arg4)> bind_member(T *instance, R(T::*func)(Arg1, Arg2, Arg3, Arg4)) { return [=](Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4) -> R { return (instance->*func)(arg1, arg2, arg3, arg4); }; }
-
-	template<typename R, typename T, typename Arg1, typename Arg2, typename Arg3, typename Arg4>
-	std::function<void(Arg1, Arg2, Arg3, Arg4)> bind_member(T *instance, void(T::*func)(Arg1, Arg2, Arg3, Arg4)) { return [=](Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4) { (instance->*func)(arg1, arg2, arg3, arg4); }; }
-
-	template<typename R, typename T, typename Arg1, typename Arg2, typename Arg3, typename Arg4, typename Arg5>
-	std::function<R(Arg1, Arg2, Arg3, Arg4, Arg5)> bind_member(T *instance, R(T::*func)(Arg1, Arg2, Arg3, Arg4, Arg5)) { return [=](Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5) -> R { return (instance->*func)(arg1, arg2, arg3, arg4, arg5); }; }
-
-	template<typename R, typename T, typename Arg1, typename Arg2, typename Arg3, typename Arg4, typename Arg5>
-	std::function<void(Arg1, Arg2, Arg3, Arg4, Arg5)> bind_member(T *instance, void(T::*func)(Arg1, Arg2, Arg3, Arg4, Arg5)) { return [=](Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5) { (instance->*func)(arg1, arg2, arg3, arg4, arg5); }; }
-
-	template<typename R, typename T, typename Arg1, typename Arg2, typename Arg3, typename Arg4, typename Arg5, typename Arg6>
-	std::function<R(Arg1, Arg2, Arg3, Arg4, Arg5, Arg6)> bind_member(T *instance, R(T::*func)(Arg1, Arg2, Arg3, Arg4, Arg5, Arg6)) { return [=](Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5, Arg6 arg6) -> R { return (instance->*func)(arg1, arg2, arg3, arg4, arg5, arg6); }; }
-
-	template<typename R, typename T, typename Arg1, typename Arg2, typename Arg3, typename Arg4, typename Arg5, typename Arg6>
-	std::function<void(Arg1, Arg2, Arg3, Arg4, Arg5, Arg6)> bind_member(T *instance, void(T::*func)(Arg1, Arg2, Arg3, Arg4, Arg5, Arg6)) { return [=](Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5, Arg6 arg6) { (instance->*func)(arg1, arg2, arg3, arg4, arg5, arg6); }; }
-
-	template<typename R, typename T, typename Arg1, typename Arg2, typename Arg3, typename Arg4, typename Arg5, typename Arg6, typename Arg7>
-	std::function<R(Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7)> bind_member(T *instance, R(T::*func)(Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7)) { return [=](Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5, Arg6 arg6, Arg7 arg7) -> R { return (instance->*func)(arg1, arg2, arg3, arg4, arg5, arg6, arg7); }; }
-
-	template<typename R, typename T, typename Arg1, typename Arg2, typename Arg3, typename Arg4, typename Arg5, typename Arg6, typename Arg7>
-	std::function<void(Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7)> bind_member(T *instance, void(T::*func)(Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7)) { return [=](Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5, Arg6 arg6, Arg7 arg7) { (instance->*func)(arg1, arg2, arg3, arg4, arg5, arg6, arg7); }; }
-
-	template<typename R, typename T, typename Arg1, typename Arg2, typename Arg3, typename Arg4, typename Arg5, typename Arg6, typename Arg7, typename Arg8>
-	std::function<R(Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8)> bind_member(T *instance, R(T::*func)(Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8)) { return [=](Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5, Arg6 arg6, Arg7 arg7, Arg8 arg8) -> R { return (instance->*func)(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8); }; }
-
-	template<typename R, typename T, typename Arg1, typename Arg2, typename Arg3, typename Arg4, typename Arg5, typename Arg6, typename Arg7, typename Arg8>
-	std::function<void(Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8)> bind_member(T *instance, void(T::*func)(Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8)) { return [=](Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5, Arg6 arg6, Arg7 arg7, Arg8 arg8) { (instance->*func)(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8); }; }
-
-	template<typename R, typename T, typename Arg1, typename Arg2, typename Arg3, typename Arg4, typename Arg5, typename Arg6, typename Arg7, typename Arg8, typename Arg9>
-	std::function<R(Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9)> bind_member(T *instance, R(T::*func)(Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9)) { return [=](Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5, Arg6 arg6, Arg7 arg7, Arg8 arg8, Arg9 arg9) -> R { return (instance->*func)(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9); }; }
-
-	template<typename R, typename T, typename Arg1, typename Arg2, typename Arg3, typename Arg4, typename Arg5, typename Arg6, typename Arg7, typename Arg8, typename Arg9>
-	std::function<void(Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9)> bind_member(T *instance, void(T::*func)(Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9)) { return [=](Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5, Arg6 arg6, Arg7 arg7, Arg8 arg8, Arg9 arg9) { (instance->*func)(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9); }; }
-
-	template<typename R, typename T, typename Arg1, typename Arg2, typename Arg3, typename Arg4, typename Arg5, typename Arg6, typename Arg7, typename Arg8, typename Arg9, typename Arg10>
-	std::function<R(Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9, Arg10)> bind_member(T *instance, R(T::*func)(Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9, Arg10)) { return [=](Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5, Arg6 arg6, Arg7 arg7, Arg8 arg8, Arg9 arg9, Arg10 arg10) -> R { return (instance->*func)(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10); }; }
-
-	template<typename R, typename T, typename Arg1, typename Arg2, typename Arg3, typename Arg4, typename Arg5, typename Arg6, typename Arg7, typename Arg8, typename Arg9, typename Arg10>
-	std::function<void(Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9, Arg10)> bind_member(T *instance, void(T::*func)(Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9, Arg10)) { return [=](Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5, Arg6 arg6, Arg7 arg7, Arg8 arg8, Arg9 arg9, Arg10 arg10) { (instance->*func)(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10); }; }
+	template<class R, class T, class... Args>
+	std::function<R(Args...)> bind_member(T* instance, R(T::*method)(Args...)) { return [=](Args && ... args) -> R { return (instance->*method)(std::forward<Args>(args)...); }; }
 }
 /// \}
