7691777d49c508d2171fd929f3911d17cf50695d
mbn <dpjudas@users.noreply.github.com>
2014-09-05 13:38:58 +0200

    Removed clanGUI

--- Examples/GUI/GUI_Layered/Sources/precomp.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <ClanLib/application.h>
-#include <ClanLib/core.h>
-#include <ClanLib/display.h>
-#include <ClanLib/gui.h>
-//#include <ClanLib/d3d.h>
-//#include <ClanLib/swrender.h>
-#include <ClanLib/gl.h>
