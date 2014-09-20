1a81fe9da4f529789eed833c87ade103d7638030
mbn <dpjudas@users.noreply.github.com>
2014-09-20 14:01:34 +0200

    Default initialization sucks when debugging!

--- Sources/API/Display/TargetProviders/primitives_array_provider.h
+++ Sources/API/Display/TargetProviders/primitives_array_provider.h
@@ -58,7 +58,7 @@ public:
 		VertexData(VertexArrayBufferProvider *array_provider, VertexAttributeDataType type, size_t offset, int size, int stride) :
 		  array_provider(array_provider), type(type), offset(offset), size(size), stride(stride) {}
 
-		VertexData() {}
+		VertexData() : array_provider(), type(), offset(), size(), stride() {}
 
 		VertexArrayBufferProvider *array_provider;
 		VertexAttributeDataType type;
