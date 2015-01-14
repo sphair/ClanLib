849f6047367d4eae1ac9d4c29c50e22e033250ae
Mark Page <rombust@server.fake>
2015-01-14 10:07:38 +0000

    Remove clanGUI from the Sound example to make it extremely basic

--- Examples/Sound/Sound/Sources/program.cpp
+++ Examples/Sound/Sound/Sources/program.cpp
@@ -35,18 +35,17 @@ int Program::main(const std::vector<std::string> &args)
 {
 
 	// Initialize ClanLib base components
-	SetupCore setup_core;
+	clan::SetupCore setup_core;
 
 	// Initialize the ClanLib display component
-	SetupDisplay setup_display;
+	clan::SetupDisplay setup_display;
 
 	// We support all display targets, in order listed here
-	SetupD3D setup_d3d;
-	SetupGL setup_gl;
-	SetupSWRender setup_swrender;
+	clan::SetupD3D setup_d3d;
+	clan::SetupGL setup_gl;
 
 	// Initialize the sound system
-	SetupSound setup_sound;
+	clan::SetupSound setup_sound;
 
 	// Start the Application
 	App app;
@@ -55,4 +54,4 @@ int Program::main(const std::vector<std::string> &args)
 }
 
 // Instantiate Application, informing it where the Program is located
-Application app(&Program::main);
+clan::Application app(&Program::main);
