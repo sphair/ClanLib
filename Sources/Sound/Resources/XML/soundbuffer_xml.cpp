4fd208ee9b6065efe4865b849d1c642952082d08
Mark Page <rombust@server.fake>
2015-01-15 08:47:37 +0000

    Move the sound XML object construction into it's own compile units

---
+++ Sources/Sound/Resources/XML/soundbuffer_xml.cpp
@@ -0,0 +1,71 @@
+/*
+**  ClanLib SDK
+**  Copyright (c) 1997-2015 The ClanLib Team
+**
+**  This software is provided 'as-is', without any express or implied
+**  warranty.  In no event will the authors be held liable for any damages
+**  arising from the use of this software.
+**
+**  Permission is granted to anyone to use this software for any purpose,
+**  including commercial applications, and to alter it and redistribute it
+**  freely, subject to the following restrictions:
+**
+**  1. The origin of this software must not be misrepresented; you must not
+**     claim that you wrote the original software. If you use this software
+**     in a product, an acknowledgment in the product documentation would be
+**     appreciated but is not required.
+**  2. Altered source versions must be plainly marked as such, and must not be
+**     misrepresented as being the original software.
+**  3. This notice may not be removed or altered from any source distribution.
+**
+**  Note: Some of the libraries ClanLib may link to may have additional
+**  requirements or restrictions.
+**
+**  File Author(s):
+**
+**    Magnus Norddahl
+*/
+
+#include "Sound/precomp.h"
+#include "API/Sound/soundbuffer.h"
+#include "API/Sound/soundbuffer_session.h"
+#include "API/Sound/soundfilter.h"
+#include "API/Sound/soundoutput.h"
+#include "API/Sound/sound.h"
+#include "API/Sound/SoundProviders/soundprovider_factory.h"
+#include "API/Core/Resources/xml_resource_document.h"
+#include "API/Core/Resources/xml_resource_node.h"
+#include "API/Core/IOData/file_system.h"
+#include "API/Core/IOData/path_help.h"
+#include "API/Core/XML/dom_element.h"
+#include "../../soundbuffer_impl.h"
+#include "API/Sound/Resources/sound_cache.h"
+
+namespace clan
+{
+
+SoundBuffer SoundBuffer::load(const std::string &id, const XMLResourceDocument &doc)
+{
+	SoundBuffer sound;
+
+	sound.impl = std::shared_ptr<SoundBuffer_Impl>(new SoundBuffer_Impl);
+
+	XMLResourceNode resource = doc.get_resource(id);
+
+	DomElement &element = resource.get_element();
+
+	std::string name = resource.get_element().get_attribute("file");
+	std::string sound_format = resource.get_element().get_attribute("format");
+	bool streamed = (element.get_attribute("stream", "no") == "yes");
+
+	sound.impl->provider = SoundProviderFactory::load(
+		PathHelp::combine(resource.get_base_path(), name),
+		streamed,
+		resource.get_file_system(), sound_format);
+
+	if (!sound.impl->provider)
+		throw Exception("Unknown sample format");
+	return sound;
+}
+
+}
