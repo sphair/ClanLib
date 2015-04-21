1d2bb34c32e2791f3c1f5e553453cc44123a9eca
Magnus Norddahl <dpjudas@users.noreply.github.com>
2015-04-21 07:41:45 +0200

    Added simple listbox view with label items

---
+++ Sources/UI/StandardViews/ListBoxView/listbox_view.cpp
@@ -0,0 +1,104 @@
+/*
+**  ClanLib SDK
+**  Copyright (c) 1997-2015 The ClanLib Team
+**
+**  This software is provided 'as-is', without any express or implied
+**  warranty.  In no event will the authors be held liable for any damages
+**  arising from the use of this software.
+**
+**  Permission is granted to anyone to use this software for any purpose,
+**  including commercial applications, and to alter it and redistribute it
+**  freely, subject to the following restrictions:
+**
+**  1. The origin of this software must not be misrepresented; you must not
+**     claim that you wrote the original software. If you use this software
+**     in a product, an acknowledgment in the product documentation would be
+**     appreciated but is not required.
+**  2. Altered source versions must be plainly marked as such, and must not be
+**     misrepresented as being the original software.
+**  3. This notice may not be removed or altered from any source distribution.
+**
+**  Note: Some of the libraries ClanLib may link to may have additional
+**  requirements or restrictions.
+**
+**  File Author(s):
+**
+**    Magnus Norddahl
+**    Mark Page
+*/
+
+#include "UI/precomp.h"
+#include "API/UI/StandardViews/listbox_view.h"
+#include "API/UI/StandardViews/label_view.h"
+#include "listbox_view_impl.h"
+
+namespace clan
+{
+	ListBoxView::ListBoxView() : impl(new ListBoxViewImpl())
+	{
+		impl->listbox = this;
+		style()->set("flex-direction: row");
+
+		slots.connect(sig_pointer_release(), impl.get(), &ListBoxViewImpl::on_pointer_release);
+	}
+
+	ListBoxView::~ListBoxView()
+	{
+	}
+	
+	std::function<void(View *)> &ListBoxView::func_style_item()
+	{
+		return impl->func_style_item;
+	}
+
+	std::function<void()> &ListBoxView::func_selection_changed()
+	{
+		return impl->func_selection_changed;
+	}
+	
+	const std::vector<std::string> &ListBoxView::items()
+	{
+		return impl->items;
+	}
+	
+	void ListBoxView::set_items(const std::vector<std::string> &items)
+	{
+		impl->items = items;
+		impl->selected_item = -1;
+		
+		auto views = subviews();
+		while (!views.empty())
+			views.back()->remove_from_super();
+		
+		for (auto &item : impl->items)
+		{
+			auto view = std::make_shared<LabelView>();
+			view->set_text(item);
+			if (impl->func_style_item)
+				impl->func_style_item(view.get());
+			add_subview(view);
+		}
+	}
+	
+	int ListBoxView::selected_item() const
+	{
+		return impl->selected_item;
+	}
+	
+	void ListBoxView::set_selected_item(int index)
+	{
+		if (index == impl->selected_item)
+			return;
+		
+		if (index < -1 || index >= impl->items.size())
+			throw Exception("Listbox index out of bounds");
+
+		if (impl->selected_item != -1)
+			subviews().at(impl->selected_item)->set_state("selected", false);
+		
+		if (index != -1)
+			subviews().at(index)->set_state("selected", true);
+		
+		impl->selected_item = index;
+	}
+}
---
+++ Sources/UI/StandardViews/ListBoxView/listbox_view_impl.cpp
@@ -0,0 +1,57 @@
+/*
+**  ClanLib SDK
+**  Copyright (c) 1997-2015 The ClanLib Team
+**
+**  This software is provided 'as-is', without any express or implied
+**  warranty.  In no event will the authors be held liable for any damages
+**  arising from the use of this software.
+**
+**  Permission is granted to anyone to use this software for any purpose,
+**  including commercial applications, and to alter it and redistribute it
+**  freely, subject to the following restrictions:
+**
+**  1. The origin of this software must not be misrepresented; you must not
+**     claim that you wrote the original software. If you use this software
+**     in a product, an acknowledgment in the product documentation would be
+**     appreciated but is not required.
+**  2. Altered source versions must be plainly marked as such, and must not be
+**     misrepresented as being the original software.
+**  3. This notice may not be removed or altered from any source distribution.
+**
+**  Note: Some of the libraries ClanLib may link to may have additional
+**  requirements or restrictions.
+**
+**  File Author(s):
+**
+**    Magnus Norddahl
+**    Mark Page
+*/
+
+#include "UI/precomp.h"
+#include "API/UI/StandardViews/listbox_view.h"
+#include "API/UI/StandardViews/text_field_view.h"
+#include "listbox_view_impl.h"
+#include "API/UI/Events/pointer_event.h"
+
+namespace clan
+{
+	void ListBoxViewImpl::on_pointer_release(PointerEvent &e)
+	{
+		if (e.phase() != EventUIPhase::bubbling)
+			return;
+		
+		auto target = e.target();
+		int index = 0;
+		for (auto &view : listbox->subviews())
+		{
+			if (view == target)
+			{
+				listbox->set_selected_item(index);
+				if (func_selection_changed)
+					func_selection_changed();
+				break;
+			}
+			index++;
+		}
+	}
+}
