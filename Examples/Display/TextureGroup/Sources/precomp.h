52129c53a93445e4b5219a11fffda6f3ebe4d416
Mark Page <rombust@server.fake>
2015-04-21 14:24:43 +0100

    Add a simple TextureGroup example to replace the TexturePacker utility

---
+++ Examples/Display/TextureGroup/Sources/precomp.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <ClanLib/core.h>
+#include <ClanLib/application.h>
+#include <ClanLib/display.h>
+#ifdef WIN32
+#include <ClanLib/d3d.h>
+#endif
+#include <ClanLib/gl.h>
+#include <ClanLib/ui.h>
+#include <cmath>
+
+
