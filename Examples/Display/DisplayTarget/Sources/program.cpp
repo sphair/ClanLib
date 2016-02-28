f2c16c7b75079bbedcd6d5b2b455bbc61c566491
Ben "root" Anderson <roothorick@gmail.com>
2016-02-28 16:27:28 -0600

    DisplayTarget example: Direct3D no longer required

--- Examples/Display/DisplayTarget/Sources/program.cpp
+++ Examples/Display/DisplayTarget/Sources/program.cpp
@@ -34,7 +34,7 @@ clan::ApplicationInstance<Program> clanapp;
 
 Program::Program()
 {
-#ifdef WIN32
+#if defined(WIN32) && !defined(__MINGW32__)
 	clan::D3DTarget::set_current();
 #else
 	clan::OpenGLTarget::set_current();
